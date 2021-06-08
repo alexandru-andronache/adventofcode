dc3a916e39ab1c570cd1df6e29867e40f9a31271
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-08 21:09:50 +0200

    2015 - day 6

--- 2015/day06/main.cpp
+++ 2015/day06/main.cpp
@@ -1,14 +1,79 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
+#include <numeric>
 
 namespace aoc2015_day06 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::vector<int>> map(1000, std::vector<int>(1000, 0));
+
+        for (const auto& line : input) {
+            std::vector<std::string> split = utils::splitString(line, " ,");
+            if (split[0] == "toggle") {
+                for (int i = std::stoi(split[2]); i <= std::stoi(split[5]); ++i) {
+                    for (int j = std::stoi(split[1]); j <= std::stoi(split[4]); ++j) {
+                        map[i][j] = (1 + map[i][j]) % 2;
+                    }
+                }
+            }
+            else if (split[1] == "on") {
+                for (int i = std::stoi(split[3]); i <= std::stoi(split[6]); ++i) {
+                    for (int j = std::stoi(split[2]); j <= std::stoi(split[5]); ++j) {
+                        map[i][j] = 1;
+                    }
+                }
+            }
+            else {
+                for (int i = std::stoi(split[3]); i <= std::stoi(split[6]); ++i) {
+                    for (int j = std::stoi(split[2]); j <= std::stoi(split[5]); ++j) {
+                        map[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        int sum = 0;
+        for (const auto& l : map) {
+            sum += std::count(l.begin(), l.end(), 1);
+        }
+        return sum;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::vector<int>> map(1000, std::vector<int>(1000, 0));
+
+        for (const auto& line : input) {
+            std::vector<std::string> split = utils::splitString(line, " ,");
+            if (split[0] == "toggle") {
+                for (int i = std::stoi(split[2]); i <= std::stoi(split[5]); ++i) {
+                    for (int j = std::stoi(split[1]); j <= std::stoi(split[4]); ++j) {
+                        map[i][j] += 2;
+                    }
+                }
+            }
+            else if (split[1] == "on") {
+                for (int i = std::stoi(split[3]); i <= std::stoi(split[6]); ++i) {
+                    for (int j = std::stoi(split[2]); j <= std::stoi(split[5]); ++j) {
+                        map[i][j] += 1;
+                    }
+                }
+            }
+            else {
+                for (int i = std::stoi(split[3]); i <= std::stoi(split[6]); ++i) {
+                    for (int j = std::stoi(split[2]); j <= std::stoi(split[5]); ++j) {
+                        map[i][j] = std::max(0, map[i][j] - 1);
+                    }
+                }
+            }
+        }
+
+        int sum = 0;
+        for (const auto& l : map) {
+            sum += std::accumulate(l.begin(), l.end(), 0);
+        }
+        return sum;
     }
 }
 
--- 2015/day06/tests/TestClass.cpp
+++ 2015/day06/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2015_day06 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2015/day06/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2015/day06/input_test.in"), 998996);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2015/day06/input.in"), 0);
+        ASSERT_EQ(part_1("../2015/day06/input.in"), 543903);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2015/day06/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2015/day06/input_test.in"), 1001996);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2015/day06/input.in"), 0);
+        ASSERT_EQ(part_2("../2015/day06/input.in"), 14687245);
     }
 }
