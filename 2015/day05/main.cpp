18d6cd7bd492e41cb88e34b42fe88938ab7c7dad
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-08 21:05:10 +0200

    2015 - day 5

--- 2015/day05/main.cpp
+++ 2015/day05/main.cpp
@@ -1,13 +1,56 @@
 #include "file.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2015_day05 {
+    bool isNicePart1(std::string_view line) {
+        int count = std::count(line.begin(), line.end(), 'a') +
+                std::count(line.begin(), line.end(), 'e') +
+                std::count(line.begin(), line.end(), 'i') +
+                std::count(line.begin(), line.end(), 'o') +
+                std::count(line.begin(), line.end(), 'u');
+        bool isSimilar = false;
+        for (int i = 1; i < line.size(); ++i) {
+            if (line[i] == line[i - 1]) {
+                isSimilar = true;
+            }
+        }
+        bool containsBadStrings = (line.find("ab") != std::string::npos) ||
+                (line.find("cd") != std::string::npos) ||
+                (line.find("pq") != std::string::npos) ||
+                (line.find("xy") != std::string::npos);
+
+        return (count >= 3) && isSimilar && !containsBadStrings;
+    }
+
+    bool isNicePart2(std::string_view line) {
+        bool foundPair = false;
+        for (int i = 0; i < line.size() - 1; ++i) {
+            foundPair = foundPair || (line.find(line.substr(i, 2), i + 2) != std::string::npos);
+        }
+        bool foundLetterRepeat = false;
+        for (int i = 0; i < line.size() - 2; ++i) {
+            foundLetterRepeat = foundLetterRepeat || (line[i] == line[i + 2]);
+        }
+        return foundPair && foundLetterRepeat;
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        int sol = 0;
+        for (const auto& line : input) {
+            sol += isNicePart1(line);
+        }
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        int sol = 0;
+        for (const auto& line : input) {
+            sol += isNicePart2(line);
+        }
+        return sol;
     }
 }
 
