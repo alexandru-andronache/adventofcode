bdfd892475e02760a80386203e80b05c4005eec3
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-18 17:25:13 +0200

    2019 - day 2

--- 2019/day02/main.cpp
+++ 2019/day02/main.cpp
@@ -1,12 +1,32 @@
 #include "file.h"
+#include "intcode.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2019_day02 {
-    int part_1(std::string_view path) {
-        return 0;
+    long long solveWithReplace(const std::vector<long long>& input, long long first, long long second) {
+        std::vector<long long> v(input);
+        v[1] = first;
+        v[2] = second;
+        intcode::IntCode<long long> calc{v, {}};
+        calc.advance();
+        return calc.values[0];
     }
 
-    int part_2(std::string_view path) {
+    long long part_1(std::string_view path) {
+        std::vector<long long> input = file::readFileAsArrayLongLong(path, ",");
+        return solveWithReplace(input, 12, 2);
+    }
+
+    long long part_2(std::string_view path) {
+        std::vector<long long> input = file::readFileAsArrayLongLong(path, ",");
+        for (int i = 0; i < 100; ++i) {
+            for (int j = 0; j < 100; ++j) {
+                if (solveWithReplace(input, i, j) == 19690720) {
+                    return i * 100 + j;
+                }
+            }
+        }
         return 0;
     }
 }
--- 2019/day02/tests/TestClass.cpp
+++ 2019/day02/tests/TestClass.cpp
@@ -2,19 +2,11 @@
 #include "../test.h"
 
 namespace aoc2019_day02 {
-    TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2019/day02/input_test.in"), 0);
-    }
-
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2019/day02/input.in"), 0);
-    }
-
-    TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2019/day02/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2019/day02/input.in"), 3716250);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2019/day02/input.in"), 0);
+        ASSERT_EQ(part_2("../2019/day02/input.in"), 6472);
     }
 }
