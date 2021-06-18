9abe23cfd134ada914c273221c8d3e9356b1a733
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-18 10:22:22 +0200

    2015 - day 19

--- 2015/day19/main.cpp
+++ 2015/day19/main.cpp
@@ -1,14 +1,46 @@
 #include "file.h"
 #include "utilities.h"
+#include "string_util.h"
 #include <iostream>
+#include <set>
+#include <string>
 
 namespace aoc2015_day19 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::pair<std::string, std::string>> replacements;
+        for (int i = 0; i < input.size() - 1; ++i) {
+            std::vector<std::string> words = utils::splitString(input[i], " ");
+            replacements.emplace_back(words[0], words[2]);
+        }
+        std::string str = *(input.end() - 1);
+
+        std::set<std::string> results;
+
+        for (const auto& r : replacements) {
+            int i = 0;
+            while (i + r.first.size() <= str.size()) {
+                if (str.substr(i, r.first.size()) == r.first) {
+                    std::string t = str;
+                    t.replace(i, r.first.size(), r.second);
+                    results.insert(t);
+                }
+                i++;
+            }
+        }
+
+        return results.size();
     }
 
     unsigned long long part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::string str = *(input.end() - 1);
+        int total = 0;
+        for (const auto& c : str) {
+            total += (c >= 'A' && c <= 'Z');
+        }
+
+        return total - 2 * string::findNumberOfSubstrings(str, "Rn") - 2 * string::findNumberOfSubstrings(str, "Y") - 1;
     }
 }
 
--- 2015/day19/tests/TestClass.cpp
+++ 2015/day19/tests/TestClass.cpp
@@ -2,19 +2,11 @@
 #include "../test.h"
 
 namespace aoc2015_day19 {
-    TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2015/day19/input_test.in"), 0);
-    }
-
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2015/day19/input.in"), 0);
-    }
-
-    TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2015/day19/input_test2.in"), 0);
+        ASSERT_EQ(part_1("../2015/day19/input.in"), 509);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2015/day19/input.in"), 0);
+        ASSERT_EQ(part_2("../2015/day19/input.in"), 195);
     }
 }
