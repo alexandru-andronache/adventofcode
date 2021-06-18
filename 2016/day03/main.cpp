f6485516add0fa4308f162dcbe47fee184ebf756
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-18 22:29:30 +0200

    2016 - day 3

--- 2016/day03/main.cpp
+++ 2016/day03/main.cpp
@@ -1,13 +1,53 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <array>
+#include <vector>
 
 namespace aoc2016_day03 {
+    bool isTriangle(const std::array<int, 3> triangle) {
+        int max = std::max({triangle[0], triangle[1], triangle[2]});
+        int sum = triangle[0] + triangle[1] + triangle[2];
+        if (sum - max > max) {
+            return true;
+        }
+        return false;
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::array<int, 3>> triangles;
+        for (const auto& line : input) {
+            std::vector<int> t = utils::splitStringToInt(line, " ");
+            triangles.push_back({t[0], t[1], t[2]});
+        }
+
+        int sol = 0;
+        for (const auto& triangle : triangles) {
+            if (isTriangle(triangle)) {
+                sol++;
+            }
+        }
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::array<int, 3>> triangles;
+        for (const auto& line : input) {
+            std::vector<int> t = utils::splitStringToInt(line, " ");
+            triangles.push_back({t[0], t[1], t[2]});
+        }
+
+        int sol = 0;
+        for (int i = 0; i < triangles.size(); i += 3) {
+            for (int j = 0; j < 3; ++j) {
+                if (isTriangle({triangles[i][j], triangles[i + 1][j], triangles[i + 2][j]})) {
+                    sol++;
+                }
+            }
+        }
+        return sol;
     }
 }
 
--- 2016/day03/tests/TestClass.cpp
+++ 2016/day03/tests/TestClass.cpp
@@ -2,19 +2,11 @@
 #include "../test.h"
 
 namespace aoc2016_day03 {
-    TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2016/day03/input_test.in"), 0);
-    }
-
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2016/day03/input.in"), 0);
-    }
-
-    TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2016/day03/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2016/day03/input.in"), 869);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2016/day03/input.in"), 0);
+        ASSERT_EQ(part_2("../2016/day03/input.in"), 1544);
     }
 }
