375b6091e2eba682fe894b6b5b031ffe96031866
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-18 09:35:10 +0200

    2015 - day 11

--- 2015/day11/main.cpp
+++ 2015/day11/main.cpp
@@ -2,20 +2,64 @@
 #include "utilities.h"
 #include <iostream>
 #include <set>
+#include <array>
 
 namespace aoc2015_day11 {
-    int part_1(std::string_view path) {
-        return 0;
+    std::string nextPassword(std::string password) {
+        int i = password.size() - 1;
+        while (password[i] == 'z') {
+            i--;
+        }
+        password[i]++;
+        for (int j = i + 1; j < password.size(); ++j) {
+            password[j] = 'a';
+        }
+        return password;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    bool isValid(std::string_view password) {
+        bool valid = false;
+        for (int i = 0; i < password.size() - 2; ++i) {
+            if (password[i] + 1 == password[i + 1] && password[i + 1] + 1 == password[i + 2]) {
+                valid = true;
+            }
+        }
+        for (const auto& c : password) {
+            valid = valid && (c != 'i' && c != 'o' && c != 'l');
+        }
+        std::array<int, 26> letters{0};
+        for (int i = 0; i < password.size() - 1; ++i) {
+            if (password[i] == password[i + 1]) {
+                letters[password[i] - 'a'] = 1;
+            }
+        }
+        valid = valid && (std::count(letters.begin(), letters.end(), 1) >= 2);
+        return valid;
+    }
+
+    std::string solve(std::string password) {
+        while (true) {
+            std::string next = nextPassword(password);
+            if (isValid(next)) {
+                return next;
+            }
+            password = next;
+        }
+    }
+
+    std::string part_1(const std::string& password) {
+        return solve(password);
+    }
+
+    std::string part_2(const std::string& password) {
+        std::string first_password = solve(password);
+        return solve(first_password);
     }
 }
 #ifndef TESTING
 int main() {
-    std::cout << "Part 1: " << aoc2015_day11::part_1("../2015/day11/input.in") << std::endl;
-    std::cout << "Part 2: " << aoc2015_day11::part_2("../2015/day11/input.in") << std::endl;
+    std::cout << "Part 1: " << aoc2015_day11::part_1("cqjxjnds") << std::endl;
+    std::cout << "Part 2: " << aoc2015_day11::part_2("cqjxjnds") << std::endl;
 
     return 0;
 }
--- 2015/day11/tests/TestClass.cpp
+++ 2015/day11/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2015_day11 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2015/day11/input_test.in"), 0);
+        ASSERT_EQ(part_1("abcdefgh"), "abcdffaa");
+    }
+
+    TEST_F(TestClass, part_1_test_2) {
+        ASSERT_EQ(part_1("ghijklmn"), "ghjaabcc");
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2015/day11/input.in"), 0);
-    }
-
-    TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2015/day11/input_test.in"), 0);
+        ASSERT_EQ(part_1("cqjxjnds"), "cqjxxyzz");
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2015/day11/input.in"), 0);
+        ASSERT_EQ(part_2("cqjxjnds"), "cqkaabcc");
     }
 }
