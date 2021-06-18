e7d04f684db044befbe77462f9975cca03674ae5
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-18 10:17:30 +0200

    2015 - day 18

--- 2015/day18/main.cpp
+++ 2015/day18/main.cpp
@@ -1,21 +1,76 @@
 #include "file.h"
 #include "utilities.h"
+#include "grid.h"
 #include <iostream>
 
 namespace aoc2015_day18 {
-    int part_1(std::string_view path) {
-        return 0;
+    int part_1(std::string_view path, int steps) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+
+        std::vector<std::vector<bool>> map(input.size(), std::vector<bool>(input[0].size(), false));
+        for (int i = 0; i < input.size(); ++i) {
+            for (int j = 0; j < input[0].size(); ++j) {
+                map[i][j] = (input[i][j] == '#');
+            }
+        }
+
+        for (int step = 0; step < steps; ++step) {
+            std::vector<std::vector<bool>> t(input.size(), std::vector<bool>(input[0].size(), false));
+            for (int i = 0; i < input.size(); ++i) {
+                for (int j = 0; j < input[0].size(); ++j) {
+                    t[i][j] = map[i][j];
+                    if ((map[i][j] && (grid::numberOfNeighbors(map, i, j, true) != 2 && grid::numberOfNeighbors(map, i, j, true) != 3)) ||
+                    (!map[i][j] && grid::numberOfNeighbors(map, i, j, true) == 3)) {
+                        t[i][j] = !map[i][j];
+                    }
+                }
+            }
+            map = t;
+        }
+        int sol = 0;
+        for (const auto& line : map) {
+            sol += std::count(line.begin(), line.end(), true);
+        }
+        return sol;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    int part_2(std::string_view path, int steps) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+
+        std::vector<std::vector<bool>> map(input.size(), std::vector<bool>(input[0].size(), false));
+        for (int i = 0; i < input.size(); ++i) {
+            for (int j = 0; j < input[0].size(); ++j) {
+                map[i][j] = (input[i][j] == '#');
+            }
+        }
+        map[0][0] = map[0].back() = map.back()[0] = map.back().back() = true;
+
+        for (int step = 0; step < steps; ++step) {
+            std::vector<std::vector<bool>> t(input.size(), std::vector<bool>(input[0].size(), false));
+            for (int i = 0; i < input.size(); ++i) {
+                for (int j = 0; j < input[0].size(); ++j) {
+                    t[i][j] = map[i][j];
+                    if ((map[i][j] && (grid::numberOfNeighbors(map, i, j, true) != 2 && grid::numberOfNeighbors(map, i, j, true) != 3)) ||
+                    (!map[i][j] && grid::numberOfNeighbors(map, i, j, true) == 3)) {
+                        t[i][j] = !map[i][j];
+                    }
+                }
+            }
+            map = t;
+            map[0][0] = map[0].back() = map.back()[0] = map.back().back() = true;
+        }
+        int sol = 0;
+        for (const auto& line : map) {
+            sol += std::count(line.begin(), line.end(), true);
+        }
+        return sol;
     }
 }
 
 #ifndef TESTING
 int main() {
-    std::cout << "Part 1: " << aoc2015_day18::part_1("../2015/day18/input.in") << std::endl;
-    std::cout << "Part 2: " << aoc2015_day18::part_2("../2015/day18/input.in") << std::endl;
+    std::cout << "Part 1: " << aoc2015_day18::part_1("../2015/day18/input.in", 100) << std::endl;
+    std::cout << "Part 2: " << aoc2015_day18::part_2("../2015/day18/input.in", 100) << std::endl;
 
     return 0;
 }
--- 2015/day18/tests/TestClass.cpp
+++ 2015/day18/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2015_day18 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2015/day18/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2015/day18/input_test.in", 4), 4);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2015/day18/input.in"), 0);
+        ASSERT_EQ(part_1("../2015/day18/input.in", 100), 1061);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2015/day18/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2015/day18/input_test.in", 5), 17);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2015/day18/input.in"), 0);
+        ASSERT_EQ(part_2("../2015/day18/input.in", 100), 1006);
     }
 }
