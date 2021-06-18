8df4d05987c7daf2f289fef40422bace1736d71b
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-18 09:40:14 +0200

    2015 - day 12

--- 2015/day12/main.cpp
+++ 2015/day12/main.cpp
@@ -4,12 +4,47 @@
 #include <set>
 
 namespace aoc2015_day12 {
+    int solve(std::string_view str, int& pos) {
+        int sum = 0;
+        bool isRed = false;
+        std::string red = ":\"red\"";
+        while (pos < str.size()) {
+            if (str[pos] == '{') {
+                pos++;
+                sum += solve(str, pos);
+            }
+            else if (str[pos] == '}') {
+                pos++;
+                return sum * (!isRed);
+            }
+            else if ((pos + red.size() < str.size()) && str.substr(pos, red.size()) == red) {
+                pos += red.size();
+                isRed = true;
+            }
+            else {
+                int prev = pos;
+                sum += utils::getNumber(str, pos);
+                pos += (prev == pos);
+            }
+        }
+        return sum * (!isRed);
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+        int sum = 0;
+        int index = 0;
+        while (index < input.size()) {
+            sum += utils::getNumber(input, index);
+            index++;
+        }
+        return sum;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+        int pos = 0;
+        return solve(input, pos);
     }
 }
 
