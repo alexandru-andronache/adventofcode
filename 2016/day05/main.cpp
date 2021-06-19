840df6dbaaf39f408b729f2b54a1bb8f40b19c52
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-19 13:21:15 +0200

    2016 - day 5

--- 2016/day05/main.cpp
+++ 2016/day05/main.cpp
@@ -1,20 +1,54 @@
 #include "file.h"
+#include "md5.h"
 #include <iostream>
 
 namespace aoc2016_day05 {
-    int part_1(std::string_view path) {
-        return 0;
+    std::string part_1(const std::string& input) {
+        int k = 0;
+        std::string sol;
+        for (int i = 0; i < 8; ++i) {
+            bool found = false;
+            while (!found) {
+                k++;
+                std::string result = md5::calculateMD5(input + std::to_string(k));
+                if (result.substr(0, 5) == "00000") {
+                    sol += result[5];
+                    found = true;
+                }
+            }
+        }
+
+        return sol;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    std::string part_2(const std::string& input) {
+        int k = 0;
+        std::string sol ="gggggggg";
+        for (int i = 0; i < 8; ++i) {
+            bool found = false;
+            while (!found) {
+                k++;
+                std::string result = md5::calculateMD5(input + std::to_string(k));
+                if (result.substr(0, 5) == "00000") {
+                    if (result[5] - '0' <= 7 && sol[result[5] - '0'] == 'g') {
+                        sol[result[5] - '0'] = result[6];
+                        found = true;
+                    }
+                    else {
+                        found = false;
+                    }
+                }
+            }
+        }
+
+        return sol;
     }
 }
 
 #ifndef TESTING
 int main() {
-    std::cout << "Part 1: " << aoc2016_day05::part_1("../2016/day05/input.in") << std::endl;
-    std::cout << "Part 2: " << aoc2016_day05::part_2("../2016/day05/input.in") << std::endl;
+    std::cout << "Part 1: " << aoc2016_day05::part_1("cxdnnyjw") << std::endl;
+    std::cout << "Part 2: " << aoc2016_day05::part_2("cxdnnyjw") << std::endl;
 
     return 0;
 }
--- 2016/day05/tests/TestClass.cpp
+++ 2016/day05/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2016_day05 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2016/day05/input_test.in"), 0);
+        ASSERT_EQ(part_1("abc"), "18f47a30");
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2016/day05/input.in"), 0);
+        ASSERT_EQ(part_1("cxdnnyjw"), "f77a0e6e");
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2016/day05/input_test.in"), 0);
+        ASSERT_EQ(part_2("abc"), "05ace8e3");
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2016/day05/input.in"), 0);
+        ASSERT_EQ(part_2("cxdnnyjw"), "999828ec");
     }
 }
