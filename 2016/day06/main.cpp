6bc5e9910eb4cedb21faadbb95c6d635f53bcb42
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-19 13:24:55 +0200

    2016 - day 6

--- 2016/day06/main.cpp
+++ 2016/day06/main.cpp
@@ -3,12 +3,40 @@
 #include <iostream>
 
 namespace aoc2016_day06 {
-    int part_1(std::string_view path) {
-        return 0;
+    std::string part_1(std::string_view path) {
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+
+        std::string sol;
+        std::vector<std::vector<int>> chars(lines[0].size(), std::vector<int>(26, 0));
+        for (const auto& line : lines) {
+            for (int i = 0; i < line.size(); ++i) {
+                chars[i][line[i] - 'a']++;
+            }
+        }
+
+        for (const auto& column : chars) {
+            auto it = std::max_element(column.begin(), column.end()) - column.begin();
+            sol += (it + 'a');
+        }
+        return sol;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    std::string part_2(std::string_view path) {
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+
+        std::string sol;
+        std::vector<std::vector<int>> chars(lines[0].size(), std::vector<int>(26, std::numeric_limits<int>::max()));
+        for (const auto& line : lines) {
+            for (int i = 0; i < line.size(); ++i) {
+                chars[i][line[i] - 'a']++;
+            }
+        }
+
+        for (const auto& column : chars) {
+            auto it = std::min_element(column.begin(), column.end()) - column.begin();
+            sol += (it + 'a');
+        }
+        return sol;
     }
 }
 
--- 2016/day06/tests/TestClass.cpp
+++ 2016/day06/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2016_day06 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2016/day06/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2016/day06/input_test.in"), "easter");
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2016/day06/input.in"), 0);
+        ASSERT_EQ(part_1("../2016/day06/input.in"), "tzstqsua");
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2016/day06/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2016/day06/input_test.in"), "advent");
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2016/day06/input.in"), 0);
+        ASSERT_EQ(part_2("../2016/day06/input.in"), "myregdnr");
     }
 }
