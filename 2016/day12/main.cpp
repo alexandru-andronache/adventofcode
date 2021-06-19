4e823e2571a0a93c8e5a35c09a77e6127f78e985
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-19 15:04:01 +0200

    2016 - day 12

--- 2016/day12/main.cpp
+++ 2016/day12/main.cpp
@@ -2,14 +2,65 @@
 #include "utilities.h"
 #include <iostream>
 #include <set>
+#include <array>
 
 namespace aoc2016_day12 {
+    int solve(const std::vector<std::vector<std::string>>& instructions, std::array<int, 4> registers = {0, 0, 0, 0}) {
+        int index = 0;
+        while (index < instructions.size()) {
+            if (instructions[index][0] == "cpy") {
+                if (instructions[index][1][0] >= '0' && instructions[index][1][0] <= '9') {
+                    registers[instructions[index][2][0] - 'a'] = std::stoi(instructions[index][1]);
+                }
+                else {
+                    registers[instructions[index][2][0] - 'a'] = registers[instructions[index][1][0] - 'a'];
+                }
+                index++;
+            }
+            else if (instructions[index][0] == "inc") {
+                registers[instructions[index][1][0] - 'a']++;
+                index++;
+            }
+            else if (instructions[index][0] == "dec") {
+                registers[instructions[index][1][0] - 'a']--;
+                index++;
+            }
+            else {
+                int value = 0;
+                if (instructions[index][1][0] >= '0' && instructions[index][1][0] <= '9') {
+                    value = std::stoi(instructions[index][1]);
+                }
+                else {
+                    value = registers[instructions[index][1][0] - 'a'];
+                }
+                if (value != 0) {
+                    index += std::stoi(instructions[index][2]);
+                }
+                else {
+                    index++;
+                }
+            }
+        }
+
+        return registers[0];
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::vector<std::string>> instructions;
+        for (const auto& line : input) {
+            instructions.push_back(utils::splitString(line, " ="));
+        }
+        return solve(instructions);
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::vector<std::string>> instructions;
+        for (const auto& line : input) {
+            instructions.push_back(utils::splitString(line, " ="));
+        }
+        return solve(instructions, {0, 0, 1, 0});
     }
 }
 
--- 2016/day12/tests/TestClass.cpp
+++ 2016/day12/tests/TestClass.cpp
@@ -3,18 +3,14 @@
 
 namespace aoc2016_day12 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2016/day12/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2016/day12/input_test.in"), 42);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2016/day12/input.in"), 0);
-    }
-
-    TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2016/day12/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2016/day12/input.in"), 318077);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2016/day12/input.in"), 0);
+        ASSERT_EQ(part_2("../2016/day12/input.in"), 9227731);
     }
 }
