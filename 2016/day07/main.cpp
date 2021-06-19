aa836959c77bf10c40c83309433d057c4a837e98
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-19 13:56:36 +0200

    2016 - day 7

--- 2016/day07/main.cpp
+++ 2016/day07/main.cpp
@@ -7,11 +7,80 @@
 
 namespace aoc2016_day07 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+
+        int sol = 0;
+        for (const auto& line : lines) {
+            bool hasAbba = false;
+            bool insideBrackets = false;
+            bool hasAbbaInsideBrackets = false;
+            for (int i = 0; i < line.size() - 3; ++i) {
+                if (line[i] == '[') {
+                    insideBrackets = true;
+                }
+                else if (line[i] == ']') {
+                    insideBrackets = false;
+                }
+                else {
+                    if (line[i] == line[i + 3] && line[i + 1] == line[i + 2] && line[i] != line[i + 1]) {
+                        if (insideBrackets) {
+                            hasAbbaInsideBrackets = true;
+                        }
+                        else {
+                            hasAbba = true;
+                        }
+                    }
+                }
+            }
+
+            if (hasAbba && !hasAbbaInsideBrackets) {
+                sol++;
+            }
+        }
+
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+
+        int sol = 0;
+        for (const auto& line : lines) {
+            std::vector<std::string> aba;
+            std::vector<std::string> bab;
+            bool insideBrackets = false;
+            for (int i = 0; i < line.size() - 2; ++i) {
+                if (line[i] == '[') {
+                    insideBrackets = true;
+                }
+                else if (line[i] == ']') {
+                    insideBrackets = false;
+                }
+                else {
+                    if (line[i] == line[i + 2] && line[i] != line[i + 1]) {
+                        if (insideBrackets) {
+                            bab.push_back(line.substr(i, 3));
+                        }
+                        else {
+                            aba.push_back(line.substr(i, 3));
+                        }
+                    }
+                }
+            }
+            bool isSol = false;
+            for (const auto& l : aba) {
+                for (const auto& p : bab) {
+                    if (l[1] == p[0] && l[0] == p[1]) {
+                        isSol = true;
+                    }
+                }
+            }
+            if (isSol) {
+                sol++;
+            }
+        }
+
+        return sol;
     }
 }
 
--- 2016/day07/tests/TestClass.cpp
+++ 2016/day07/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2016_day07 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2016/day07/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2016/day07/input_test.in"), 2);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2016/day07/input.in"), 0);
+        ASSERT_EQ(part_1("../2016/day07/input.in"), 110);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2016/day07/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2016/day07/input_test2.in"), 3);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2016/day07/input.in"), 0);
+        ASSERT_EQ(part_2("../2016/day07/input.in"), 242);
     }
 }
