d71f76ab35e58ae4ad295305da37062203977d55
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-20 09:18:53 +0200

    2016 - day 18

--- 2016/day18/main.cpp
+++ 2016/day18/main.cpp
@@ -3,19 +3,56 @@
 #include <iostream>
 
 namespace aoc2016_day18 {
-    int part_1(std::string_view path) {
-        return 0;
+    int solve(const std::string& input, int rows) {
+        std::vector<std::vector<bool>> map(rows, std::vector<bool>(input.size(), false));
+        for (int i = 0; i < input.size(); ++i) {
+            if (input[i] == '.') {
+                map[0][i] = true;
+            }
+        }
+
+        for (int i = 0; i < rows - 1; ++i) {
+            for (int j = 0; j < map[i].size(); ++j) {
+                if (j > 0 && !map[i][j - 1] && !map[i][j] && ((j + 1 < map[i].size() && map[i][j + 1]) || j + 1 == map[i].size())) {
+                    map[i + 1][j] = false;
+                }
+                else if (((j > 0 && map[i][j - 1]) || j == 0)  && !map[i][j] && (j + 1 < map[i].size()) && !map[i][j + 1]) {
+                    map[i + 1][j] = false;
+                }
+                else if (j > 0 && !map[i][j - 1] && map[i][j] && (((j + 1 < map[i].size()) && map[i][j + 1]) || j + 1 == map[i].size())) {
+                    map[i + 1][j] = false;
+                }
+                else if (((j > 0 && map[i][j - 1]) || (j == 0)) && map[i][j] && (j + 1 < map[i].size()) && !map[i][j + 1]) {
+                    map[i + 1][j] = false;
+                }
+                else {
+                    map[i + 1][j] = true;
+                }
+            }
+        }
+
+        int sol = 0;
+        for (const auto& row : map) {
+            for (const auto& el : row) {
+                sol += el;
+            }
+        }
+        return sol;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    int part_1(std::string_view path, int rows) {
+        return solve(file::readFileAsString(path), rows);
+    }
+
+    int part_2(std::string_view path, int rows) {
+        return solve(file::readFileAsString(path), rows);
     }
 }
 
 #ifndef TESTING
 int main() {
-    std::cout << "Part 1: " << aoc2016_day18::part_1("../2016/day18/input.in") << std::endl;
-    std::cout << "Part 2: " << aoc2016_day18::part_2("../2016/day18/input.in") << std::endl;
+    std::cout << "Part 1: " << aoc2016_day18::part_1("../2016/day18/input.in", 40) << std::endl;
+    std::cout << "Part 2: " << aoc2016_day18::part_2("../2016/day18/input.in", 400000) << std::endl;
 
     return 0;
 }
--- 2016/day18/tests/TestClass.cpp
+++ 2016/day18/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2016_day18 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2016/day18/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2016/day18/input_test.in", 3), 6);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2016/day18/input.in"), 0);
+        ASSERT_EQ(part_1("../2016/day18/input.in", 40), 1987);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2016/day18/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2016/day18/input_test2.in", 10), 38);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2016/day18/input.in"), 0);
+        ASSERT_EQ(part_2("../2016/day18/input.in", 400000), 19984714);
     }
 }
