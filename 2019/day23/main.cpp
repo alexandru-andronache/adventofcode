5a867491634a85a688a9efea4bde6ed10bb41710
alexandru-andronache <al3x.andronache@gmail.com>
2021-06-29 09:42:37 +0200

    2019 - day 23

--- 2019/day23/main.cpp
+++ 2019/day23/main.cpp
@@ -1,14 +1,120 @@
 #include "file.h"
-#include "utilities.h"
+#include "intcode.h"
 #include <iostream>
 
 namespace aoc2019_day23 {
-    int part_1(std::string_view path) {
-        return 0;
+    struct packet {
+        unsigned long long x {0};
+        unsigned long long y {0};
+    };
+
+    unsigned long long part_1(std::string_view path) {
+        std::vector<unsigned long long> input = file::readFileAsArrayULL(path, ",");
+        std::vector<intcode::IntCode<unsigned long long>> computers(50, {input, {}});
+        std::vector<std::vector<packet>> packets(50);
+
+        for (int i = 0; i < 50; ++i) {
+            computers[i].inputIDs.push_back(i);
+        }
+
+        while (true) {
+            for (int i = 0; i < 50; ++i) {
+                auto exit = computers[i].advance();
+                while (!exit.needInput && !exit.halt) {
+                    unsigned long long id = exit.returnValue;
+                    packet p;
+                    p.x = computers[i].advance().returnValue;
+                    p.y = computers[i].advance().returnValue;
+
+                    if (id == 255) {
+                        return p.y;
+                    }
+                    else if (id < 50) {
+                        packets[id].push_back(p);
+                    }
+                    exit = computers[i].advance();
+                }
+            }
+
+            for (int i = 0; i < 50; ++i) {
+                for (const auto& p : packets[i]) {
+                    computers[i].inputIDs.push_back(p.x);
+                    computers[i].inputIDs.push_back(p.y);
+                }
+                packets[i].clear();
+            }
+            for (int i = 0; i < 50; ++i) {
+                if (computers[i].inputIDs.empty()) {
+                    computers[i].inputIDs.push_back(-1);
+                }
+            }
+        }
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long part_2(std::string_view path) {
+        std::vector<unsigned long long> input = file::readFileAsArrayULL(path, ",");
+        std::vector<intcode::IntCode<unsigned long long>> computers(50, {input, {}});
+        std::vector<std::vector<packet>> packets(50);
+        packet NAT;
+        unsigned long long formerNATy = 0;
+
+        for (int i = 0; i < 50; ++i) {
+            computers[i].inputIDs.push_back(i);
+            computers[i].advance();
+            computers[i].inputIDs.push_back(-1);
+        }
+
+        while (true) {
+            for (int i = 0; i < 50; ++i) {
+                auto exit = computers[i].advance();
+                while (!exit.needInput && !exit.halt) {
+                    unsigned long long id = exit.returnValue;
+                    packet p;
+                    p.x = computers[i].advance().returnValue;
+                    p.y = computers[i].advance().returnValue;
+
+                    if (id == 255) {
+                        NAT = p;
+                    }
+                    else if (id < 50) {
+                        packets[id].push_back(p);
+                    }
+                    exit = computers[i].advance();
+                }
+            }
+
+            for (int i = 0; i < 50; ++i) {
+                for (const auto& p : packets[i]) {
+                    computers[i].inputIDs.push_back(p.x);
+                    computers[i].inputIDs.push_back(p.y);
+                }
+                packets[i].clear();
+            }
+            bool idle = true;
+            for (const auto& c : computers) {
+                if (!c.inputIDs.empty()) {
+                    idle = false;
+                }
+            }
+            for (const auto& p : packets) {
+                if (!p.empty()) {
+                    idle = false;
+                }
+            }
+            if (idle) {
+                if (formerNATy == NAT.y) {
+                    return formerNATy;
+                }
+                formerNATy = NAT.y;
+                computers[0].inputIDs.push_back(NAT.x);
+                computers[0].inputIDs.push_back(NAT.y);
+            }
+            for (int i = 0; i < 50; ++i) {
+                if (computers[i].inputIDs.empty()) {
+                    computers[i].inputIDs.push_back(-1);
+                }
+            }
+        }
     }
 }
 
--- 2019/day23/tests/TestClass.cpp
+++ 2019/day23/tests/TestClass.cpp
@@ -2,19 +2,11 @@
 #include "../test.h"
 
 namespace aoc2019_day23 {
-    TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2019/day23/input_test.in"), 0);
-    }
-
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2019/day23/input.in"), 0);
-    }
-
-    TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2019/day23/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2019/day23/input.in"), 24922);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2019/day23/input.in"), 0);
+        ASSERT_EQ(part_2("../2019/day23/input.in"), 19478);
     }
 }
