1f5c02843fea12c9b9b7b4c851512e5743fbc32f
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-01 09:09:45 +0200

    2019 - day 25

--- 2019/day25/main.cpp
+++ 2019/day25/main.cpp
@@ -1,9 +1,74 @@
 #include "file.h"
-#include "utilities.h"
+#include "intcode.h"
 #include <iostream>
 
 namespace aoc2019_day25 {
     int part_1(std::string_view path) {
+        std::vector<unsigned long long> input = file::readFileAsArrayULL(path, ",");
+        std::vector<std::string> commands = {"east\n",
+                                             "take antenna\n",
+                                             "west\n",
+                                             "north\n",
+                                             "take weather machine\n",
+                                             "north\n",
+                                             "east\n",
+                                             "take spool of cat6\n",
+                                             "east\n",
+                                             "north\n",
+                                             "west\n",
+                                             "north\n",
+                                             "south\n",
+                                             "east\n",
+                                             "east\n",
+                                             "north\n",
+                                             "north\n",
+                                             "south\n",
+                                             "south\n",
+                                             "south\n",
+                                             "east\n",
+                                             "south\n",
+                                             "north\n",
+                                             "west\n",
+                                             "north\n",
+                                             "west\n",
+                                             "south\n",
+                                             "south\n",
+                                             "take mug\n",
+                                             "north\n",
+                                             "west\n",
+                                             "south\n",
+                                             "south\n",
+                                             "east\n"};
+
+        intcode::IntCode<unsigned long long> droid(input, {});
+        auto exit = droid.advance();
+        while ((char)exit.returnValue != '?') {
+            exit = droid.advance();
+        }
+        exit = droid.advance();
+
+        for (const auto& command : commands) {
+            for (const auto& c : command) {
+                droid.inputIDs.push_back(c);
+            }
+            std::string output;
+            for (int i = 0; i < 200; ++i) {
+                exit = droid.advance();
+                output += (char)exit.returnValue;
+            }
+            if (output.find("Analysis complete! You may proceed.") != std::string::npos) {
+                std::string newOutput;
+                for (int i = 0; i < 200; ++i) {
+                    exit = droid.advance();
+                    newOutput += (char)exit.returnValue;
+                }
+                std::string stringBeforeResult = "typing ";
+                auto pos = newOutput.find(stringBeforeResult);
+                auto numberEnd = newOutput.find(' ', pos + stringBeforeResult.size());
+                return std::stoi(newOutput.substr(pos + stringBeforeResult.size(), numberEnd - stringBeforeResult.size() - pos));
+            }
+        }
+
         return 0;
     }
 }
--- 2019/day25/tests/TestClass.cpp
+++ 2019/day25/tests/TestClass.cpp
@@ -2,11 +2,7 @@
 #include "../test.h"
 
 namespace aoc2019_day25 {
-    TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2019/day25/input_test.in"), 0);
-    }
-
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2019/day25/input.in"), 0);
+        ASSERT_EQ(part_1("../2019/day25/input.in"), 805307408);
     }
 }
