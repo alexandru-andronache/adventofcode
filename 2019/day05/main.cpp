d4b6fc27a30e22a8c257591da654bc37c9de16e0
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-01 15:48:23 +0200

    2019 - day 5

--- 2019/day05/main.cpp
+++ 2019/day05/main.cpp
@@ -1,13 +1,29 @@
 #include "file.h"
+#include "intcode.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2019_day05 {
-    int part_1(std::string_view path) {
-        return 0;
+    int part_1(std::string_view path, long long inputId = 1) {
+        std::vector<long long> input = file::readFileAsArrayLongLong(path, ",");
+
+        intcode::IntCode<long long> calc(input, {inputId}, 0);
+        intcode::IntCode<long long>::exitState exit {0, 0, false};
+        while (exit.returnValue == 0 && !exit.halt) {
+            exit = calc.advance();
+        }
+        return exit.returnValue;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    int part_2(std::string_view path, long long inputId = 5) {
+        std::vector<long long> input = file::readFileAsArrayLongLong(path, ",");
+
+        intcode::IntCode<long long> calc(input, {inputId}, 0);
+        intcode::IntCode<long long>::exitState exit {0, 0, false};
+        while (exit.returnValue == 0 && !exit.halt) {
+            exit = calc.advance();
+        }
+        return exit.returnValue;
     }
 }
 
