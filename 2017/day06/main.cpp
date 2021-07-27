d44a4a27e1e8d8acd807d4000444711c94c3ea08
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-27 16:13:08 +0200

    2017 - day 06

--- 2017/day06/main.cpp
+++ 2017/day06/main.cpp
@@ -1,14 +1,58 @@
 #include "file.h"
-#include "utilities.h"
 #include <iostream>
+#include <vector>
+#include <set>
 
 namespace aoc2017_day06 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<int> input = file::readFileAsArrayInt(path);
+
+        std::set<std::vector<int>> states;
+        while (states.insert(input).second) {
+            auto it = std::max_element(input.begin(), input.end());
+            int max = *it;
+            *it = 0;
+            for (int i = 0; i < max; ++i) {
+                it++;
+                if (it == input.end()) {
+                    it = input.begin();
+                }
+                (*it)++;
+            }
+        }
+        return states.size();
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<int> input = file::readFileAsArrayInt(path);
+
+        std::set<std::vector<int>> states;
+        while (states.insert(input).second) {
+            auto it = std::max_element(input.begin(), input.end());
+            int max = *it;
+            *it = 0;
+            for (int i = 0; i < max; ++i) {
+                it++;
+                if (it == input.end()) {
+                    it = input.begin();
+                }
+                (*it)++;
+            }
+        }
+        std::set<std::vector<int>> loop;
+        while (loop.insert(input).second) {
+            auto it = std::max_element(input.begin(), input.end());
+            int max = *it;
+            *it = 0;
+            for (int i = 0; i < max; ++i) {
+                it++;
+                if (it == input.end()) {
+                    it = input.begin();
+                }
+                (*it)++;
+            }
+        }
+        return loop.size();
     }
 }
 
--- 2017/day06/tests/TestClass.cpp
+++ 2017/day06/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2017_day06 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2017/day06/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2017/day06/input_test.in"), 5);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2017/day06/input.in"), 0);
+        ASSERT_EQ(part_1("../2017/day06/input.in"), 5042);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2017/day06/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2017/day06/input_test.in"), 4);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2017/day06/input.in"), 0);
+        ASSERT_EQ(part_2("../2017/day06/input.in"), 1086);
     }
 }
