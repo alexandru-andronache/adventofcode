2d0e874942e5e0ca5e800f9ae65a94448a5fe4f1
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-27 16:34:03 +0200

    2017 - day 9

--- 2017/day09/main.cpp
+++ 2017/day09/main.cpp
@@ -3,11 +3,64 @@
 
 namespace aoc2017_day09 {
     int part_1(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+
+        int sum = 0;
+        int score = 0;
+        bool isGarbage = false;
+        bool ignoreNext = false;
+        for (const auto& c : input) {
+            if (ignoreNext) {
+                ignoreNext = false;
+            }
+            else if (c == '!') {
+                ignoreNext = true;
+            }
+            else if (isGarbage && c != '>') {
+
+            }
+            else if (c == '>') {
+                isGarbage = false;
+            }
+            else if (c == '<' && !isGarbage) {
+                isGarbage = true;
+            }
+            else if (c == '{') {
+                score++;
+            }
+            else if (c == '}') {
+                sum += score;
+                score--;
+            }
+        }
+
+        return sum;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+
+        int nrGarbage = 0;
+        bool isGarbage = false;
+        bool ignoreNext = false;
+        for (const auto& c : input) {
+            if (ignoreNext) {
+                ignoreNext = false;
+            }
+            else if (c == '!') {
+                ignoreNext = true;
+            }
+            else if (isGarbage && c != '>') {
+                nrGarbage++;
+            }
+            else if (c == '>') {
+                isGarbage = false;
+            }
+            else if (c == '<' && !isGarbage) {
+                isGarbage = true;
+            }
+        }
+        return nrGarbage;
     }
 }
 
--- 2017/day09/tests/TestClass.cpp
+++ 2017/day09/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2017_day09 {
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2017/day09/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2017/day09/input_test.in"), 3);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2017/day09/input.in"), 0);
+        ASSERT_EQ(part_1("../2017/day09/input.in"), 14204);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2017/day09/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2017/day09/input_test2.in"), 10);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2017/day09/input.in"), 0);
+        ASSERT_EQ(part_2("../2017/day09/input.in"), 6622);
     }
 }
