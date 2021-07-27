19c5e0158b79bd06345fe96b9555b14e580fae36
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-27 16:07:23 +0200

    2017 - day 5

--- 2017/day05/main.cpp
+++ 2017/day05/main.cpp
@@ -1,13 +1,39 @@
 #include "file.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2017_day05 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<int> input = file::readFileAsArrayInt(path);
+
+        int start = 0;
+        int steps = 0;
+        while (start < input.size()) {
+            int newStart = start + input[start];
+            input[start]++;
+            start = newStart;
+            steps++;
+        }
+        return steps;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<int> input = file::readFileAsArrayInt(path);
+
+        int start = 0;
+        int steps = 0;
+        while (start < input.size()) {
+            int newStart = start + input[start];
+            if (input[start] >= 3) {
+                input[start]--;
+            }
+            else {
+                input[start]++;
+            }
+            start = newStart;
+            steps++;
+        }
+        return steps;
     }
 }
 
