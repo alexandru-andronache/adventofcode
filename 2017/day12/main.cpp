c507562c0cad74422192199ce0a78bd459a00eab
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-27 17:06:09 +0200

    2017 - day 12

--- 2017/day12/main.cpp
+++ 2017/day12/main.cpp
@@ -4,12 +4,78 @@
 #include <set>
 
 namespace aoc2017_day12 {
-    int part_1(std::string_view path) {
-        return 0;
+    int part_1(std::string_view path, int id = 0) {
+        std::vector<std::pair<int, std::vector<int>>> input;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        for (const auto& line : lines) {
+            std::vector<int> tokens = utils::splitStringToInt(line, "  <->,");
+            input.push_back({tokens[0], {tokens.begin() + 1, tokens.end()}});
+        }
+
+        const int SIZE = 3000;
+        std::vector<std::vector<bool>> pipes(SIZE, std::vector<bool>(SIZE, false));
+        for (const auto& line : input) {
+            for (const auto& x : line.second) {
+                pipes[line.first][x] = pipes[x][line.first] = true;
+            }
+        }
+
+        std::vector<bool> visited(SIZE, false);
+        std::vector<int> sol;
+        sol.push_back(0);
+        visited[0] = true;
+        int i = 0;
+        while (i < sol.size()) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (pipes[sol[i]][j] && !visited[j]) {
+                    sol.push_back(j);
+                    visited[j] = true;
+                }
+            }
+            i++;
+        }
+        return sol.size();
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::pair<int, std::vector<int>>> input;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        for (const auto& line : lines) {
+            std::vector<int> tokens = utils::splitStringToInt(line, "  <->,");
+            input.push_back({tokens[0], {tokens.begin() + 1, tokens.end()}});
+        }
+
+        const int SIZE = 3000;
+        int max = 0;
+        std::vector<std::vector<bool>> pipes(SIZE, std::vector<bool>(SIZE, false));
+        for (const auto& line : input) {
+            for (const auto& x : line.second) {
+                pipes[line.first][x] = pipes[x][line.first] = true;
+            }
+            max = std::max(max, line.first);
+        }
+
+        std::vector<bool> visited(SIZE, false);
+        int groups = 0;
+        for (int k = 0; k <= max; ++k) {
+            if (!visited[k]) {
+                groups++;
+                std::vector<int> sol;
+                sol.push_back(k);
+                visited[k] = true;
+                int i = 0;
+                while (i < sol.size()) {
+                    for (int j = 0; j < SIZE; ++j) {
+                        if (pipes[sol[i]][j] && !visited[j]) {
+                            sol.push_back(j);
+                            visited[j] = true;
+                        }
+                    }
+                    i++;
+                }
+            }
+        }
+        return groups;
     }
 }
 
--- 2017/day12/test.h
+++ 2017/day12/test.h
@@ -3,6 +3,6 @@
 #include <string>
 
 namespace aoc2017_day12 {
-    int part_1(std::string_view path);
+    int part_1(std::string_view path, int id = 0);
     int part_2(std::string_view path);
 }
