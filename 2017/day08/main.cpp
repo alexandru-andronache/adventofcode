2e68e2f281af8dea0fa694e6d76b33679b746c7e
alexandru-andronache <al3x.andronache@gmail.com>
2021-07-27 16:29:56 +0200

    2017 - day 8

--- 2017/day08/main.cpp
+++ 2017/day08/main.cpp
@@ -1,13 +1,112 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <vector>
+#include <map>
 
 namespace aoc2017_day08 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::tuple<std::string, std::string, int, std::string, std::string, int>> input;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        for (const auto& line : lines) {
+            std::vector<std::string> tokens = utils::splitString(line, " ");
+            input.push_back({tokens[0], tokens[1], std::stoi(tokens[2]), tokens[4], tokens[5], std::stoi(tokens[6])});
+        }
+
+        std::map<std::string, int> registers;
+        for (const auto& line : input) {
+            const auto& [value1, op, nr1, value2, comp, nr2] = line;
+            int value = 0;
+            if (registers.find(value2) != registers.end()) {
+                value = registers[value2];
+            }
+            bool conditionTrue = false;
+            if (comp.compare("<") == 0 && value < nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare("<=") == 0 && value <= nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare(">") == 0 && value > nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare(">=") == 0 && value >= nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare("==") == 0 && value == nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare("!=") == 0 && value != nr2) {
+                conditionTrue = true;
+            }
+            if (conditionTrue) {
+                if (registers.find(value1) == registers.end()) {
+                    registers.insert({value1, 0});
+                }
+                if (op.compare("inc") == 0) {
+                    registers[value1] += nr1;
+                }
+                else {
+                    registers[value1] -= nr1;
+                }
+            }
+        }
+
+        return std::max_element(registers.begin(), registers.end(), [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        })->second;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::tuple<std::string, std::string, int, std::string, std::string, int>> input;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        for (const auto& line : lines) {
+            std::vector<std::string> tokens = utils::splitString(line, " ");
+            input.push_back({tokens[0], tokens[1], std::stoi(tokens[2]), tokens[4], tokens[5], std::stoi(tokens[6])});
+        }
+
+        std::map<std::string, int> registers;
+        int max = std::numeric_limits<int>::min();
+        for (const auto& line : input) {
+            const auto& [value1, op, nr1, value2, comp, nr2] = line;
+            int value = 0;
+            if (registers.find(value2) != registers.end()) {
+                value = registers[value2];
+            }
+            bool conditionTrue = false;
+            if (comp.compare("<") == 0 && value < nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare("<=") == 0 && value <= nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare(">") == 0 && value > nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare(">=") == 0 && value >= nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare("==") == 0 && value == nr2) {
+                conditionTrue = true;
+            }
+            else if (comp.compare("!=") == 0 && value != nr2) {
+                conditionTrue = true;
+            }
+            if (conditionTrue) {
+                if (registers.find(value1) == registers.end()) {
+                    registers.insert({value1, 0});
+                }
+                if (op.compare("inc") == 0) {
+                    registers[value1] += nr1;
+                }
+                else {
+                    registers[value1] -= nr1;
+                }
+                max = std::max(max, registers[value1]);
+            }
+        }
+
+        return max;
     }
 }
 
--- 2017/day08/tests/TestClass.cpp
+++ 2017/day08/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2017_day08{
     TEST_F(TestClass, part_1_test) {
-        ASSERT_EQ(part_1("../2017/day08/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2017/day08/input_test.in"), 1);
     }
 
     TEST_F(TestClass, part_1_real_test) {
-        ASSERT_EQ(part_1("../2017/day08/input.in"), 0);
+        ASSERT_EQ(part_1("../2017/day08/input.in"), 5966);
     }
 
     TEST_F(TestClass, part_2_test) {
-        ASSERT_EQ(part_2("../2017/day08/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2017/day08/input_test.in"), 10);
     }
 
     TEST_F(TestClass, part_2_real_test) {
-        ASSERT_EQ(part_2("../2017/day08/input.in"), 0);
+        ASSERT_EQ(part_2("../2017/day08/input.in"), 6347);
     }
 }
