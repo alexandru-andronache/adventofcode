4f67509de66a6b136744117c8c1c8e13810768e8
alexandru-andronache <al3x.andronache@gmail.com>
2021-08-03 18:20:03 +0200

    2018 - day 5

--- 2018/day05/main.cpp
+++ 2018/day05/main.cpp
@@ -2,12 +2,37 @@
 #include <iostream>
 
 namespace aoc2018_day05 {
+    std::string solvePolymer(const std::string& input, char polymer = '\0') {
+        std::string result = "";
+        int i = 0;
+        while (i < input.size()) {
+            if (result.size() > 0 && std::abs(result.back() - input[i]) == std::abs('A' - 'a')) {
+                result.pop_back();
+            }
+            else {
+                if (std::abs(input[i] - polymer) != std::abs('A' - 'a') && input[i] != polymer) {
+                    result += input[i];
+                }
+            }
+            i++;
+        }
+        return result;
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+        return solvePolymer(input).size();
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+
+        int min = input.size();
+        for (int i = 'a'; i <= 'z'; ++i) {
+            int l = solvePolymer(input, i).size();
+            min = std::min(min, l);
+        }
+        return min;
     }
 }
 
--- 2018/day05/tests/TestClass.cpp
+++ 2018/day05/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2018_day05 {
     TEST_F(Tests2018Day05, part_1_test) {
-        ASSERT_EQ(part_1("../2018/day05/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2018/day05/input_test.in"), 10);
     }
 
     TEST_F(Tests2018Day05, part_1_real_test) {
-        ASSERT_EQ(part_1("../2018/day05/input.in"), 0);
+        ASSERT_EQ(part_1("../2018/day05/input.in"), 9060);
     }
 
     TEST_F(Tests2018Day05, part_2_test) {
-        ASSERT_EQ(part_2("../2018/day05/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2018/day05/input_test.in"), 4);
     }
 
     TEST_F(Tests2018Day05, part_2_real_test) {
-        ASSERT_EQ(part_2("../2018/day05/input.in"), 0);
+        ASSERT_EQ(part_2("../2018/day05/input.in"), 6310);
     }
 }
