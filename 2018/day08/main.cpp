d9eadcad134b6238afd468ec9fcd11aa1f144e4d
alexandru-andronache <al3x.andronache@gmail.com>
2021-08-03 20:08:46 +0200

    2018 - day 8

--- 2018/day08/main.cpp
+++ 2018/day08/main.cpp
@@ -1,13 +1,59 @@
 #include "file.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2018_day08 {
+    void parseSol1(const std::vector<int>& numbers, int& index, int& sum) {
+        int metadata = numbers[index + 1];
+        int child = numbers[index];
+        index += 2;
+        for (int i = 0; i < child; ++i) {
+            parseSol1(numbers, index, sum);
+        }
+        for (int i = index; i < index + metadata; ++i) {
+            sum += numbers[i];
+        }
+        index += metadata;
+    }
+
+    int parseSol2(const std::vector<int>& numbers, int& index) {
+        int metadata = numbers[index + 1];
+        int nrChildren = numbers[index];
+        std::vector<int> values;
+        index += 2;
+        for (int i = 0; i < nrChildren; ++i) {
+            values.push_back(parseSol2(numbers, index));
+        }
+        int value = 0;
+        if (nrChildren == 0) {
+            for (int i = index; i < index + metadata; ++i) {
+                value += numbers[i];
+            }
+        }
+        else {
+            for (int i = index; i < index + metadata; ++i) {
+                if (numbers[i] - 1 < nrChildren) {
+                    value += values[numbers[i] - 1];
+                }
+            }
+        }
+        index += metadata;
+        return value;
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<int> numbers = file::readFileAsArrayInt(path);
+        int i = 0;
+        int sum = 0;
+        parseSol1 (numbers, i, sum);
+        return sum;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<int> numbers = file::readFileAsArrayInt(path);
+
+        int i = 0;
+        return parseSol2(numbers, i);
     }
 }
 
--- 2018/day08/tests/TestClass.cpp
+++ 2018/day08/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2018_day08{
     TEST_F(Tests2018Day08, part_1_test) {
-        ASSERT_EQ(part_1("../2018/day08/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2018/day08/input_test.in"), 138);
     }
 
     TEST_F(Tests2018Day08, part_1_real_test) {
-        ASSERT_EQ(part_1("../2018/day08/input.in"), 0);
+        ASSERT_EQ(part_1("../2018/day08/input.in"), 43825);
     }
 
     TEST_F(Tests2018Day08, part_2_test) {
-        ASSERT_EQ(part_2("../2018/day08/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2018/day08/input_test.in"), 66);
     }
 
     TEST_F(Tests2018Day08, part_2_real_test) {
-        ASSERT_EQ(part_2("../2018/day08/input.in"), 0);
+        ASSERT_EQ(part_2("../2018/day08/input.in"), 19276);
     }
 }
