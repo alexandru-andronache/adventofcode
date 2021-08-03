88f2002b8f8d7602ae8eaaaf396c38acb8a469b3
alexandru-andronache <al3x.andronache@gmail.com>
2021-08-03 07:06:14 +0200

    2018 - day 1

--- 2018/day01/main.cpp
+++ 2018/day01/main.cpp
@@ -1,14 +1,28 @@
 #include "file.h"
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <set>
 
 namespace aoc2018_day01 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<int> input = file::readFileAsArrayInt(path);
+        return std::accumulate(input.begin(), input.end(), 0);
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<int> input = file::readFileAsArrayInt(path);
+        std::set<int> partialSums;
+        int sum = 0;
+        while (true) {
+            for (auto x : input) {
+                partialSums.insert(sum);
+                sum += x;
+                if (partialSums.find(sum) != partialSums.end()) {
+                    return sum;
+                }
+            }
+        }
     }
 }
 
--- 2018/day01/tests/TestClass.cpp
+++ 2018/day01/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2018_day01 {
     TEST_F(Tests2018Day01, part_1_test) {
-        ASSERT_EQ(part_1("../2018/day01/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2018/day01/input_test.in"), 4);
     }
 
     TEST_F(Tests2018Day01, part_1_real_test) {
-        ASSERT_EQ(part_1("../2018/day01/input.in"), 0);
+        ASSERT_EQ(part_1("../2018/day01/input.in"), 484);
     }
 
     TEST_F(Tests2018Day01, part_2_test) {
-        ASSERT_EQ(part_2("../2018/day01/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2018/day01/input_test.in"), 10);
     }
 
     TEST_F(Tests2018Day01, part_2_real_test) {
-        ASSERT_EQ(part_2("../2018/day01/input.in"), 0);
+        ASSERT_EQ(part_2("../2018/day01/input.in"), 367);
     }
 }
