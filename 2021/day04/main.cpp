c3622c86cf32bd95972d8a29b17bb7b4fec6c9f1
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-04 06:39:25 +0100

    2021 - day 4

--- 2021/day04/main.cpp
+++ 2021/day04/main.cpp
@@ -1,12 +1,145 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2021_day04 {
+
+    int calculateScore(const std::vector<std::vector<std::vector<int>>>& cards,
+                       const std::vector<std::vector<std::vector<bool>>>& mark,
+                       int index,
+                       int lastNumber) {
+        int sum = 0;
+        for (int i = 0; i < cards[0].size(); ++i) {
+            for (int j = 0; j < cards[0][0].size(); ++j) {
+                if (!mark[index][i][j]) {
+                    sum += cards[index][i][j];
+                }
+            }
+        }
+        return sum * lastNumber;
+    }
+
     int part_1(std::string_view path) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<int> bingo = utils::splitStringToInt(input[0], ", ");
+        std::vector<std::vector<std::vector<int>>> cards;
+
+        for (int i = 2; i < input.size(); i += 6) {
+            std::vector<std::vector<int>> card;
+            for (int j = i; j < i + 5; ++j) {
+                std::vector<int> line = utils::splitStringToInt(input[j], " ");
+                card.push_back(line);
+            }
+            cards.push_back(card);
+        }
+
+        std::vector<std::vector<std::vector<bool>>> mark(cards.size(), std::vector<std::vector<bool>>(cards[0].size(), std::vector<bool>(cards[0][0].size(), false)));
+
+        int index = 0;
+        while (true) {
+            for (int i = 0; i < cards.size(); ++i) {
+                for (int j = 0; j < cards[0].size(); ++j) {
+                    for (int k = 0; k < cards[0][0].size(); ++k) {
+                        if (cards[i][j][k] == bingo[index]) {
+                            mark[i][j][k] = true;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < cards.size(); ++i) {
+                for (int j = 0; j < cards[0].size(); ++j) {
+                    bool finished = true;
+                    for (int k = 0; k < cards[0][0].size(); ++k) {
+                        if (!mark[i][j][k]) {
+                            finished = false;
+                        }
+                    }
+                    if (finished) {
+                        return calculateScore(cards, mark, i, bingo[index]);
+                    }
+                }
+                for (int k = 0; k < cards[0][0].size(); ++k) {
+                    bool finished = true;
+                    for (int j = 0; j < cards[0].size(); ++j) {
+                        if (!mark[i][j][k]) {
+                            finished = false;
+                        }
+                    }
+                    if (finished) {
+                        return calculateScore(cards, mark, i, bingo[index]);
+                    }
+                }
+            }
+            index++;
+        }
+
         return 0;
     }
 
     int part_2(std::string_view path) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<int> bingo = utils::splitStringToInt(input[0], ", ");
+        std::vector<std::vector<std::vector<int>>> cards;
+
+        for (int i = 2; i < input.size(); i += 6) {
+            std::vector<std::vector<int>> card;
+            for (int j = i; j < i + 5; ++j) {
+                std::vector<int> line = utils::splitStringToInt(input[j], " ");
+                card.push_back(line);
+            }
+            cards.push_back(card);
+        }
+
+        std::vector<std::vector<std::vector<bool>>> mark(cards.size(), std::vector<std::vector<bool>>(cards[0].size(), std::vector<bool>(cards[0][0].size(), false)));
+        std::vector<bool> cardsCompleted(cards.size(), false);
+
+        int index = 0;
+        while (true) {
+            for (int i = 0; i < cards.size(); ++i) {
+                for (int j = 0; j < cards[0].size(); ++j) {
+                    for (int k = 0; k < cards[0][0].size(); ++k) {
+                        if (cards[i][j][k] == bingo[index]) {
+                            mark[i][j][k] = true;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < cards.size(); ++i) {
+                for (int j = 0; j < cards[0].size(); ++j) {
+                    bool finished = true;
+                    for (int k = 0; k < cards[0][0].size(); ++k) {
+                        if (!mark[i][j][k]) {
+                            finished = false;
+                        }
+                    }
+                    if (finished) {
+                        cardsCompleted[i] = true;
+                        if (std::count(cardsCompleted.begin(), cardsCompleted.end(), false) == 0) {
+                            return calculateScore(cards, mark, i, bingo[index]);
+                        }
+                    }
+                }
+                for (int k = 0; k < cards[0][0].size(); ++k) {
+                    bool finished = true;
+                    for (int j = 0; j < cards[0].size(); ++j) {
+                        if (!mark[i][j][k]) {
+                            finished = false;
+                        }
+                    }
+                    if (finished) {
+                        cardsCompleted[i] = true;
+                        if (std::count(cardsCompleted.begin(), cardsCompleted.end(), false) == 0) {
+                            return calculateScore(cards, mark, i, bingo[index]);
+                        }
+                    }
+                }
+            }
+            index++;
+        }
+
         return 0;
     }
 }
--- 2021/day04/tests/TestClass.cpp
+++ 2021/day04/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day04 {
     TEST_F(Tests2021Day04, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day04/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day04/input_test.in"), 4512);
     }
 
     TEST_F(Tests2021Day04, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day04/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day04/input.in"), 64084);
     }
 
     TEST_F(Tests2021Day04, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day04/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day04/input_test.in"), 1924);
     }
 
     TEST_F(Tests2021Day04, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day04/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day04/input.in"), 12833);
     }
 }
