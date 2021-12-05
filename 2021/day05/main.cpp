740b902042ea442eac69173d34779ac1ec2ce58a
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-05 07:02:20 +0100

    2021 - day 5

--- 2021/day05/main.cpp
+++ 2021/day05/main.cpp
@@ -1,13 +1,77 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2021_day05 {
+    const int N = 1000;
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+
+        std::vector<std::vector<int>> map(N, std::vector<int>(N, 0));
+
+        for (const auto& line : input) {
+            std::vector<int> tokens = utils::splitStringToInt(line, " ,->");
+            if (tokens[0] == tokens[2]) {
+                for (int i = std::min(tokens[1], tokens[3]); i <= std::max(tokens[1], tokens[3]); ++i) {
+                    map[tokens[0]][i]++;
+                }
+            }
+            else if (tokens[1] == tokens[3]) {
+                for (int i = std::min(tokens[0], tokens[2]); i <= std::max(tokens[0], tokens[2]); ++i) {
+                    map[i][tokens[1]]++;
+                }
+            }
+        }
+        int sol = 0;
+        for (const auto& line : map) {
+            sol += std::count_if(line.begin(), line.end(), [](const auto& x) {
+                return x > 1;
+            });
+        }
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+
+        std::vector<std::vector<int>> map(N, std::vector<int>(N, 0));
+
+        for (const auto& line : input) {
+            std::vector<int> tokens = utils::splitStringToInt(line, " ,->");
+            if (tokens[0] == tokens[2]) {
+                for (int i = std::min(tokens[1], tokens[3]); i <= std::max(tokens[1], tokens[3]); ++i) {
+                    map[tokens[0]][i]++;
+                }
+            }
+            else if (tokens[1] == tokens[3]) {
+                for (int i = std::min(tokens[0], tokens[2]); i <= std::max(tokens[0], tokens[2]); ++i) {
+                    map[i][tokens[1]]++;
+                }
+            }
+            else {
+                if (tokens[0] > tokens[2]) {
+                    std::swap(tokens[0], tokens[2]);
+                    std::swap(tokens[1], tokens[3]);
+                }
+                for (int i = tokens[0], j = 0; i <= tokens[2]; ++i, ++j) {
+                    if (tokens[1] < tokens[3]) {
+                        map[i][tokens[1] + j]++;
+                    }
+                    else {
+                        map[i][tokens[1] - j]++;
+                    }
+                }
+            }
+        }
+        int sol = 0;
+        for (const auto& line : map) {
+            sol += std::count_if(line.begin(), line.end(), [](const auto& x) {
+                return x > 1;
+            });
+        }
+        return sol;
     }
 }
 
--- 2021/day05/tests/TestClass.cpp
+++ 2021/day05/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day05 {
     TEST_F(Tests2021Day05, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day05/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day05/input_test.in"), 5);
     }
 
     TEST_F(Tests2021Day05, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day05/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day05/input.in"), 6666);
     }
 
     TEST_F(Tests2021Day05, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day05/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day05/input_test.in"), 12);
     }
 
     TEST_F(Tests2021Day05, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day05/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day05/input.in"), 19081);
     }
 }
