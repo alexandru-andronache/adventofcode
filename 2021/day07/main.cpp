d2a0b72a52ec007678d6b630bd3e2bf0e45dc322
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-07 06:13:08 +0100

    2021 - day 7

--- 2021/day07/main.cpp
+++ 2021/day07/main.cpp
@@ -1,17 +1,41 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
-#include <map>
-#include <stack>
-#include <set>
+#include <vector>
 
 namespace aoc2021_day07 {
     int part_1(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+        std::vector<int> numbers = utils::splitStringToInt(input, " ,");
+
+        int min = std::numeric_limits<int>::max();
+        for (int fuel = 1; fuel <= 1000; ++fuel) {
+            int n = 0;
+            for (const auto& f : numbers) {
+                n += std::abs(f - fuel);
+            }
+            min = std::min(min, n);
+        }
+        return min;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long sum(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    unsigned long long part_2(std::string_view path) {
+        std::string input = file::readFileAsString(path);
+        std::vector<int> numbers = utils::splitStringToInt(input, " ,");
+
+        unsigned long long min = std::numeric_limits<unsigned long long>::max();
+        for (int fuel = 1; fuel <= 1000; ++fuel) {
+            unsigned long long n = 0;
+            for (const auto& f : numbers) {
+                n += sum(std::abs(f - fuel));
+            }
+            min = std::min(min, n);
+        }
+        return min;
     }
 }
 
--- 2021/day07/tests/TestClass.cpp
+++ 2021/day07/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day07 {
     TEST_F(Tests2021Day07, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day07/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day07/input_test.in"), 37);
     }
 
     TEST_F(Tests2021Day07, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day07/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day07/input.in"), 344535);
     }
 
     TEST_F(Tests2021Day07, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day07/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day07/input_test.in"), 168);
     }
 
     TEST_F(Tests2021Day07, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day07/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day07/input.in"), 95581659);
     }
 }
