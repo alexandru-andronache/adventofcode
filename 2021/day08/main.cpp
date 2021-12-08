413c4164e7876239318744c87b01c489c101cba4
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-08 07:45:27 +0100

    2021 - day 8

--- 2021/day08/main.cpp
+++ 2021/day08/main.cpp
@@ -1,13 +1,159 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2021_day08 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        int sol = 0;
+        for (const auto& line : input) {
+            std::vector<std::string> tokens = utils::splitString(line, "|");
+            std::vector<std::string> words = utils::splitString(tokens[1], " ");
+            sol += std::count_if(words.begin(), words.end(), [](const auto& l) {
+                return l.size() == 2 || l.size() == 3 || l.size() == 4 || l.size() == 7;
+            });
+        }
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        int sum = 0;
+
+        for (const auto& line : input) {
+            std::vector<std::string> words = utils::splitString(line, " |");
+            // find 1
+            char topRight = 'x', bottomRight = 'x', middle = 'x', top = 'x', bottom = 'x', topLeft = 'x', bottomLeft = 'x';
+            for (const auto& w : words) {
+                if (w.size() == 2) {
+                    topRight = w[0];
+                    bottomRight = w[1];
+                }
+            }
+
+            // find 7
+            for (const auto& w : words) {
+                if (w.size() == 3) {
+                    for (const auto& l : w) {
+                        if (l != topRight && l != bottomRight) {
+                            top = l;
+                        }
+                    }
+                }
+            }
+
+            // find 3
+            for (const auto& w : words) {
+                if (w.size() == 5) {
+                    if (std::count(w.begin(), w.end(), topRight) == 1 &&
+                        std::count(w.begin(), w.end(), bottomRight) == 1) {
+                        for (const auto& l : w) {
+                            if (l != topRight && l != bottomRight && l != top) {
+                                if (middle != 'x') {
+                                    if (bottom == 'x') {
+                                        bottom = l;
+                                    }
+                                }
+                                else {
+                                    middle = l;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // find 4
+            for (const auto& w : words) {
+                if (w.size() == 4) {
+                    for (const auto& l : w) {
+                        if (l == middle) {
+                            for (const auto& l1 : w) {
+                                if (l1 != topRight && l1 != bottomRight && l1 != middle) {
+                                    topLeft = l1;
+                                }
+                            }
+                        }
+                        else if (l == bottom) {
+                            std::swap(middle, bottom);
+                            for (const auto& l1 : w) {
+                                if (l1 != topRight && l1 != bottomRight && l1 != middle) {
+                                    topLeft = l1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // find 0
+            for (const auto& w : words) {
+                if (w.size() == 6) {
+                    if (std::count(w.begin(), w.end(), middle) == 0) {
+                        for (const auto& l : w) {
+                            if (l != top && l != bottom && l != topRight && l != bottomRight && l != topLeft) {
+                                bottomLeft = l;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // find 5
+            for (const auto& w : words) {
+                if (w.size() == 5) {
+                    if (std::count(w.begin(), w.end(), topLeft) == 1) {
+                        if (std::count(w.begin(), w.end(), topRight) == 1) {
+                            std::swap(topRight, bottomRight);
+                        }
+                    }
+                }
+            }
+
+            int nr = 0;
+            for (int i = 10; i < words.size(); ++i) {
+                if (words[i].size() == 2) { // nr 1
+                    nr = nr * 10 + 1;
+                }
+                else if (words[i].size() == 3) { // nr 7
+                    nr = nr * 10 + 7;
+                }
+                else if (words[i].size() == 4) { // 4
+                    nr = nr * 10 + 4;
+                }
+                else if (words[i].size() == 5) { // 2, 3, 5
+                    if (std::count(words[i].begin(), words[i].end(), topRight) == 1 &&
+                        std::count(words[i].begin(), words[i].end(), bottomRight) == 1) { // 3
+                        nr = nr * 10 + 3;
+                    }
+                    else if (std::count(words[i].begin(), words[i].end(), topRight) == 1 &&
+                             std::count(words[i].begin(), words[i].end(), bottomLeft) == 1) { // 2
+                        nr = nr * 10 + 2;
+                    }
+                    else { // 5
+                        nr = nr * 10 + 5;
+                    }
+                }
+                else if (words[i].size() == 6) { //0, 6, 9
+                    if (std::count(words[i].begin(), words[i].end(), middle) == 0) { // 0
+                        nr = nr * 10 + 0;
+                    }
+                    else if (std::count(words[i].begin(), words[i].end(), topRight) == 1) { // 9
+                        nr = nr * 10 + 9;
+                    }
+                    else { //6
+                        nr = nr * 10 + 6;
+                    }
+                }
+                else if (words[i].size() == 7) { // 8
+                    nr = nr * 10 + 8;
+                }
+            }
+            sum += nr;
+
+        }
+        return sum;
     }
 }
 
--- 2021/day08/tests/TestClass.cpp
+++ 2021/day08/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day08{
     TEST_F(Tests2021Day08, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day08/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day08/input_test.in"), 26);
     }
 
     TEST_F(Tests2021Day08, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day08/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day08/input.in"), 449);
     }
 
     TEST_F(Tests2021Day08, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day08/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day08/input_test.in"), 61229);
     }
 
     TEST_F(Tests2021Day08, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day08/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day08/input.in"), 968175);
     }
 }
