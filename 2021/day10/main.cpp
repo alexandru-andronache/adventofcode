ca00b2b92cc0bd42c0e56750a17986f9cefa6474
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-10 08:05:53 +0100

    2021 - day 10

--- 2021/day10/main.cpp
+++ 2021/day10/main.cpp
@@ -4,12 +4,89 @@
 #include <array>
 
 namespace aoc2021_day10 {
-    int part_1(std::string_view path) {
+    struct match {
+        char begin;
+        char end;
+        int score;
+    };
+
+    int processCorruptString(std::string_view line) {
+        std::string str;
+        std::vector<match> braces{{'(', ')', 3},
+                                  {'[', ']', 57},
+                                  {'{', '}', 1197},
+                                  {'<', '>', 25137}};
+        for (const auto& c : line) {
+            auto start = std::find_if(braces.begin(), braces.end(), [c](const auto& x) {
+                return x.begin == c;
+            });
+            if (start != braces.end()) {
+                str += c;
+            }
+            else {
+                auto it = std::find_if(braces.begin(), braces.end(), [c](const auto& x) {
+                    return x.end == c;
+                });
+                if (str.back() == it->begin) {
+                    str.pop_back();
+                }
+                else {
+                    return it->score;
+                }
+            }
+        }
         return 0;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long processIncompleteString(std::string_view line) {
+        std::string str;
+        std::vector<match> braces{{'(', ')', 1},
+                                  {'[', ']', 2},
+                                  {'{', '}', 3},
+                                  {'<', '>', 4}};
+        for (const auto& c : line) {
+            auto start = std::find_if(braces.begin(), braces.end(), [c](const auto& x) {
+                return x.begin == c;
+            });
+            if (start != braces.end()) {
+                str += c;
+            }
+            else {
+                str.pop_back();
+            }
+        }
+
+        unsigned long long score = 0;
+        for (int i = str.size() - 1; i >= 0; --i) {
+            auto start = std::find_if(braces.begin(), braces.end(), [str, i](const auto& x) {
+                return x.begin == str[i];
+            });
+            score = score * 5 + start->score;
+        }
+        return score;
+    }
+
+    unsigned long long part_1(std::string_view path) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+
+        unsigned long long sum = 0;
+        for (const auto & line : input) {
+            sum += processCorruptString(line);
+        }
+        return sum;
+    }
+
+    unsigned long long part_2(std::string_view path) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+
+        std::vector<unsigned long long> scores;
+        for (const auto & line : input) {
+            if (!processCorruptString(line)) {
+                scores.push_back(processIncompleteString(line));
+            }
+        }
+        std::sort(scores.begin(), scores.end());
+        return scores[scores.size() / 2];
     }
 }
 
--- 2021/day10/tests/TestClass.cpp
+++ 2021/day10/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day10{
     TEST_F(Tests2021Day10, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day10/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day10/input_test.in"), 26397);
     }
 
     TEST_F(Tests2021Day10, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day10/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day10/input.in"), 266301);
     }
 
     TEST_F(Tests2021Day10, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day10/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day10/input_test.in"), 288957);
     }
 
     TEST_F(Tests2021Day10, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day10/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day10/input.in"), 3404870164);
     }
 }
