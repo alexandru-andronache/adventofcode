5f67565db72d70d168a4e6fe72b452349910b54d
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-11 10:47:32 +0100

    2021 - day 11

--- 2021/day11/main.cpp
+++ 2021/day11/main.cpp
@@ -1,40 +1,40 @@
 #include "file.h"
-#include "direction.h"
 #include "utilities.h"
 #include <iostream>
 #include <vector>
 #include <set>
 
 namespace aoc2021_day11 {
-    std::vector<std::vector<int>> map;
-    std::set<std::pair<int, int>> explosions;
-    int sol1 = 0;
-    void calculate(int x, int y) {
-        std::vector<utils::point> neighbours = utils::getListOfNeighboursAllDirections(x, y, map);
-        for (const auto& n : neighbours) {
-            map[n.x][n.y]++;
-            if (map[n.x][n.y] > 9 && explosions.count({n.x, n.y}) == 0) {
-                explosions.insert({n.x, n.y});
-                map[n.x][n.y] = 0;
-                sol1++;
-                calculate(n.x, n.y);
-            }
-        }
-    }
-    int part_1(std::string_view path) {
-        map = file::readFileAsMap(path);
-        sol1 = 0;
 
-        for (int step = 0; step < 100; ++step) {
+    int part_1(std::string_view path, int steps = 100) {
+        std::vector<std::vector<int>> map = file::readFileAsMap(path);
+        std::set<std::pair<int, int>> explosions;
+        int sol = 0;
+
+        for (int step = 0; step < steps; ++step) {
             explosions.clear();
             for (int i = 0; i < map.size(); ++i) {
                 for (int j = 0; j < map[i].size(); ++j) {
                     map[i][j]++;
                     if (map[i][j] > 9 && explosions.count({i, j}) == 0) {
                         explosions.insert({i, j});
-                        map[i][j] = 0;
-                        sol1++;
-                        calculate(i, j);
+                        std::vector<utils::point> flash {{i, j}};
+                        while (!flash.empty()) {
+                            std::vector<utils::point> newFlash;
+                            for (const auto& f : flash) {
+                                map[f.x][f.y] = 0;
+                                sol++;
+                                std::vector<utils::point> neighbours = utils::getListOfNeighboursAllDirections(f.x, f.y, map);
+                                for (const auto& n : neighbours) {
+                                    map[n.x][n.y]++;
+                                    if (map[n.x][n.y] > 9 && explosions.count({n.x, n.y}) == 0) {
+                                        explosions.insert({n.x, n.y});
+                                        newFlash.push_back({n.x, n.y});
+                                    }
+                                }
+                            }
+                            flash = newFlash;
+                        }
                     }
                 }
             }
@@ -42,30 +42,45 @@ namespace aoc2021_day11 {
                 map[e.first][e.second] = 0;
             }
         }
-        return sol1;
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        map = file::readFileAsMap(path);
-
+        std::vector<std::vector<int>> map = file::readFileAsMap(path);
+        std::set<std::pair<int, int>> explosions;
         int step = 0;
+
         while (true) {
-            step++;
             explosions.clear();
             for (int i = 0; i < map.size(); ++i) {
                 for (int j = 0; j < map[i].size(); ++j) {
                     map[i][j]++;
                     if (map[i][j] > 9 && explosions.count({i, j}) == 0) {
                         explosions.insert({i, j});
-                        map[i][j] = 0;
-                        calculate(i, j);
+                        std::vector<utils::point> flash {{i, j}};
+                        while (!flash.empty()) {
+                            std::vector<utils::point> newFlash;
+                            for (const auto& f : flash) {
+                                map[f.x][f.y] = 0;
+                                std::vector<utils::point> neighbours = utils::getListOfNeighboursAllDirections(f.x, f.y, map);
+                                for (const auto& n : neighbours) {
+                                    map[n.x][n.y]++;
+                                    if (map[n.x][n.y] > 9 && explosions.count({n.x, n.y}) == 0) {
+                                        explosions.insert({n.x, n.y});
+                                        newFlash.push_back({n.x, n.y});
+                                    }
+                                }
+                            }
+                            flash = newFlash;
+                        }
                     }
                 }
             }
             for (const auto& e : explosions) {
                 map[e.first][e.second] = 0;
             }
-            if (explosions.size() == 100) {
+            step++;
+            if (explosions.size() == map.size() * map[0].size()) {
                 return step;
             }
         }
