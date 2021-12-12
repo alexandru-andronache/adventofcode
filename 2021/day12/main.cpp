fe7b592372ff3e1480cd8f10f68effe0cde62dbd
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-12 07:24:57 +0100

    2021 - day 12

--- 2021/day12/main.cpp
+++ 2021/day12/main.cpp
@@ -1,15 +1,177 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
-#include <set>
+#include <vector>
+#include <map>
 
 namespace aoc2021_day12 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        int sol = 0;
+        std::vector<std::vector<bool>> map(30, std::vector<bool>(30, false));
+        std::map<std::string, int> caves;
+        std::map<int, std::string> cavesReverse;
+        int index = 0;
+        for (const auto& line : lines) {
+            std::vector<std::string> tokens = utils::splitString(line, "-");
+            if (caves.find(tokens[0]) == caves.end()) {
+                caves.insert({tokens[0], index});
+                cavesReverse.insert({index, tokens[0]});
+                index++;
+            }
+            if (caves.find(tokens[1]) == caves.end()) {
+                caves.insert({tokens[1], index});
+                cavesReverse.insert({index, tokens[1]});
+
+                index++;
+            }
+            map[caves[tokens[0]]][caves[tokens[1]]] = true;
+            map[caves[tokens[1]]][caves[tokens[0]]] = true;
+        }
+
+        int start = caves["start"];
+        int end = caves["end"];
+
+        struct history {
+            history(const std::vector<int>& his) {
+                h = his;
+            }
+            history(std::vector<int>& his) {
+                h = his;
+            }
+            std::vector<int> h;
+        };
+
+        for (int i = 0; i < 30; ++i) {
+            if (map[start][i]) {
+                std::vector<history> paths {{{start, i}}};
+                while (!paths.empty()) {
+                    std::vector<history> newPaths;
+                    for (const auto& p : paths) {
+                        for (int j = 0; j < 30; ++j) {
+                            if (map[p.h.back()][j]) {
+                                std::string cave = cavesReverse[j];
+                                bool allUpperCase = true;
+                                for (const auto& c : cave) {
+                                    if (c >= 'a' && c <= 'z') {
+                                        allUpperCase = false;
+                                    }
+                                }
+                                if (j == end) {
+                                    sol++;
+                                }
+                                else if (j == start) {
+
+                                }
+                                else if (allUpperCase) {
+                                    std::vector<int> temp = p.h;
+                                    temp.push_back(j);
+                                    newPaths.push_back(temp);
+                                }
+                                else {
+                                    std::vector<int> temp = p.h;
+                                    if (std::find(temp.begin(), temp.end(), j) == temp.end()) {
+                                        temp.push_back(j);
+                                        newPaths.push_back(temp);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    paths = newPaths;
+                }
+            }
+        }
+
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        int sol = 0;
+        std::vector<std::vector<bool>> map(30, std::vector<bool>(30, false));
+        std::map<std::string, int> caves;
+        std::map<int, std::string> cavesReverse;
+        int index = 0;
+        for (const auto& line : lines) {
+            std::vector<std::string> tokens = utils::splitString(line, "-");
+            if (caves.find(tokens[0]) == caves.end()) {
+                caves.insert({tokens[0], index});
+                cavesReverse.insert({index, tokens[0]});
+                index++;
+            }
+            if (caves.find(tokens[1]) == caves.end()) {
+                caves.insert({tokens[1], index});
+                cavesReverse.insert({index, tokens[1]});
+
+                index++;
+            }
+            map[caves[tokens[0]]][caves[tokens[1]]] = true;
+            map[caves[tokens[1]]][caves[tokens[0]]] = true;
+        }
+
+        int start = caves["start"];
+        int end = caves["end"];
+
+        struct history {
+            history(const std::vector<int>& his, bool small) {
+                h = his;
+                smallCaveVisitedTwice = small;
+            }
+            history(std::vector<int>& his, bool small) {
+                h = his;
+                smallCaveVisitedTwice = small;
+            }
+            std::vector<int> h;
+            bool smallCaveVisitedTwice;
+        };
+
+        for (int i = 0; i < 30; ++i) {
+            if (map[start][i]) {
+                std::vector<history> paths {{{start, i}, false}};
+                while (!paths.empty()) {
+                    std::vector<history> newPaths;
+                    for (const auto& p : paths) {
+                        for (int j = 0; j < 30; ++j) {
+                            if (map[p.h.back()][j]) {
+                                std::string cave = cavesReverse[j];
+                                bool allUpperCase = true;
+                                for (const auto& c : cave) {
+                                    if (c >= 'a' && c <= 'z') {
+                                        allUpperCase = false;
+                                    }
+                                }
+                                if (j == end) {
+                                    sol++;
+                                }
+                                else if (j == start) {
+
+                                }
+                                else if (allUpperCase) {
+                                    std::vector<int> temp = p.h;
+                                    temp.push_back(j);
+                                    newPaths.push_back({temp, p.smallCaveVisitedTwice});
+                                }
+                                else {
+                                    std::vector<int> temp = p.h;
+                                    if (std::find(temp.begin(), temp.end(), j) == temp.end()) {
+                                        temp.push_back(j);
+                                        newPaths.push_back({temp, p.smallCaveVisitedTwice});
+                                    }
+                                    else if (std::count(temp.begin(), temp.end(), j) == 1 && !p.smallCaveVisitedTwice) {
+                                        temp.push_back(j);
+                                        newPaths.push_back({temp, true});
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    paths = newPaths;
+                }
+            }
+        }
+
+        return sol;
     }
 }
 
--- 2021/day12/tests/TestClass.cpp
+++ 2021/day12/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day12 {
     TEST_F(Tests2021Day12, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day12/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day12/input_test.in"), 10);
     }
 
     TEST_F(Tests2021Day12, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day12/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day12/input.in"), 3497);
     }
 
     TEST_F(Tests2021Day12, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day12/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day12/input_test.in"), 36);
     }
 
     TEST_F(Tests2021Day12, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day12/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day12/input.in"), 93686);
     }
 }
