bd44165fd38ea79ea3c2b0ff4a4869fbb086d3c3
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-15 08:44:56 +0100

    2021 - day 15

--- 2021/day15/main.cpp
+++ 2021/day15/main.cpp
@@ -4,19 +4,18 @@
 #include <vector>
 
 namespace aoc2021_day15 {
-    int part_1(std::string_view path) {
-        std::vector<std::vector<int>> map = file::readFileAsMap(path);
+    int solve(int startX, int startY, int endX, int endY, const std::vector<std::vector<int>>& map, int extra) {
         std::vector<std::vector<int>> risk(map.size(), std::vector<int>(map[0].size(), std::numeric_limits<int>::max()));
-        risk[0][0] = 0;
+        risk[startX][startY] = 0;
 
-        std::vector<utils::point> positions {{0, 0}};
+        std::vector<utils::point> positions {{startX, startY}};
         while (!positions.empty()) {
             std::vector<utils::point> newPositions;
             for (const auto& position : positions) {
                 std::vector<utils::point> neighbours = utils::getListOfNeighbours4Directions(position.x, position.y, map);
                 for (const auto& n : neighbours) {
-                    if (risk[position.x][position.y] + map[n.x][n.y] < risk[n.x][n.y]) {
-                        risk[n.x][n.y] = risk[position.x][position.y] + map[n.x][n.y];
+                    if (risk[position.x][position.y] + map[n.x][n.y] + extra < risk[n.x][n.y]) {
+                        risk[n.x][n.y] = risk[position.x][position.y] + map[n.x][n.y] + extra;
                         newPositions.push_back({n.x, n.y});
                     }
                 }
@@ -24,7 +23,12 @@ namespace aoc2021_day15 {
             positions = newPositions;
         }
 
-        return risk.back().back();
+        return risk[endX][endY];
+    }
+
+    int part_1(std::string_view path) {
+        std::vector<std::vector<int>> map = file::readFileAsMap(path);
+        return solve(0, 0, map.size() - 1, map[0].size() - 1, map, 0);
     }
 
     int part_2(std::string_view path) {
