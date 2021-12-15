fca98c578f58411756a1155cb3c90f27ddd17fae
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-15 08:30:37 +0100

    2021 - day 15

--- 2021/day15/main.cpp
+++ 2021/day15/main.cpp
@@ -1,13 +1,68 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2021_day15 {
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::vector<int>> map = file::readFileAsMap(path);
+        std::vector<std::vector<int>> risk(map.size(), std::vector<int>(map[0].size(), std::numeric_limits<int>::max()));
+        risk[0][0] = 0;
+
+        std::vector<utils::point> positions {{0, 0}};
+        while (!positions.empty()) {
+            std::vector<utils::point> newPositions;
+            for (const auto& position : positions) {
+                std::vector<utils::point> neighbours = utils::getListOfNeighbours4Directions(position.x, position.y, map);
+                for (const auto& n : neighbours) {
+                    if (risk[position.x][position.y] + map[n.x][n.y] < risk[n.x][n.y]) {
+                        risk[n.x][n.y] = risk[position.x][position.y] + map[n.x][n.y];
+                        newPositions.push_back({n.x, n.y});
+                    }
+                }
+            }
+            positions = newPositions;
+        }
+
+        return risk.back().back();
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::vector<int>> map = file::readFileAsMap(path);
+        std::vector<std::vector<int>> newMap;
+        int x = map.size();
+        int y = map[0].size();
+        for (int i = 0; i < x * 5; ++i) {
+            std::vector<int> line;
+            for (int j = 0; j < y * 5; ++j) {
+                int val = (map[i % x][j % y] + i / x + j / y);
+                if (val > 9) {
+                    val = val % 9;
+                }
+                line.push_back(val);
+            }
+            newMap.push_back(line);
+        }
+        map = newMap;
+        std::vector<std::vector<int>> risk(map.size(), std::vector<int>(map[0].size(), std::numeric_limits<int>::max()));
+        risk[0][0] = 0;
+
+        std::vector<utils::point> positions {{0, 0}};
+        while (!positions.empty()) {
+            std::vector<utils::point> newPositions;
+            for (const auto& position : positions) {
+                std::vector<utils::point> neighbours = utils::getListOfNeighbours4Directions(position.x, position.y, map);
+                for (const auto& n : neighbours) {
+                    if (risk[position.x][position.y] + map[n.x][n.y] < risk[n.x][n.y]) {
+                        risk[n.x][n.y] = risk[position.x][position.y] + map[n.x][n.y];
+                        newPositions.push_back({n.x, n.y});
+                    }
+                }
+            }
+            positions = newPositions;
+        }
+
+        return risk.back().back();
     }
 }
 
--- 2021/day15/tests/TestClass.cpp
+++ 2021/day15/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day15 {
     TEST_F(Tests2021Day15, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day15/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day15/input_test.in"), 40);
     }
 
     TEST_F(Tests2021Day15, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day15/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day15/input.in"), 824);
     }
 
     TEST_F(Tests2021Day15, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day15/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day15/input_test.in"), 315);
     }
 
     TEST_F(Tests2021Day15, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day15/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day15/input.in"), 3063);
     }
 }
