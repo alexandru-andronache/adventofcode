b000f8827f529220a1fe88ddc7d86e7ef5c77bc0
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-17 06:50:19 +0100

    2021 - day 17

--- 2021/day17/main.cpp
+++ 2021/day17/main.cpp
@@ -7,30 +7,25 @@ namespace aoc2021_day17 {
     int part_1(std::string_view path) {
         std::string input = file::readFileAsString(path);
         std::vector<std::string> tokens = utils::splitString(input, " : =.,");
-        int xStart = std::stoi(tokens[3].c_str());
-        int xEnd = std::stoi(tokens[4].c_str());
-        int yStart = std::stoi(tokens[6].c_str());
-        int yEnd = std::stoi(tokens[7].c_str());
+        int startX = std::stoi(tokens[3]);
+        int endX = std::stoi(tokens[4]);
+        int startY = std::stoi(tokens[6]);
+        int endY = std::stoi(tokens[7]);
         int sol = std::numeric_limits<int>::min();
 
-        for (int x = -1000; x <= 1000; ++x) {
-            for (int y = -1000; y <= 1000; ++y) {
+        for (int x = 0; x <= endX; ++x) {
+            for (int y = startY; y <= 150; ++y) {
                 int posX = 0, posY = 0;
                 int speedX = x, speedY = y;
                 int maxY = std::numeric_limits<int>::min();
                 bool wasSolution = false;
-                while (posY >= yStart && !wasSolution) {
+                while (posY >= startY && !wasSolution) {
                     posX += speedX;
                     posY += speedY;
                     maxY = std::max(maxY, posY);
-                    if (speedX < 0) {
-                        speedX++;
-                    }
-                    else if (speedX != 0) {
-                        speedX--;
-                    }
+                    speedX = std::max(0, speedX - 1);
                     speedY--;
-                    if (posX >= xStart && posX <= xEnd && posY >= yStart && posY <= yEnd) {
+                    if (posX >= startX && posX <= endX && posY >= startY && posY <= endY) {
                         wasSolution = true;
                         sol = std::max(sol, maxY);
                     }
@@ -44,31 +39,25 @@ namespace aoc2021_day17 {
     int part_2(std::string_view path) {
         std::string input = file::readFileAsString(path);
         std::vector<std::string> tokens = utils::splitString(input, " : =.,");
-        int xStart = std::stoi(tokens[3].c_str());
-        int xEnd = std::stoi(tokens[4].c_str());
-        int yStart = std::stoi(tokens[6].c_str());
-        int yEnd = std::stoi(tokens[7].c_str());
+        int startX = std::stoi(tokens[3]);
+        int endX = std::stoi(tokens[4]);
+        int startY = std::stoi(tokens[6]);
+        int endY = std::stoi(tokens[7]);
         int sol = 0;
 
-        for (int x = -1000; x <= 1000; ++x) {
-            for (int y = -1000; y <= 1000; ++y) {
+        for (int x = 0; x <= endX; ++x) {
+            for (int y = startY; y <= 150; ++y) {
                 int posX = 0, posY = 0;
                 int speedX = x, speedY = y;
-                bool wasSolution = false;
-                while (posY >= yStart && !wasSolution) {
+                bool solution = false;
+                while (posY >= startY && !solution) {
                     posX += speedX;
                     posY += speedY;
-                    if (speedX < 0) {
-                        speedX++;
-                    }
-                    else if (speedX != 0) {
-                        speedX--;
-                    }
+                    speedX = std::max(0, speedX - 1);
                     speedY--;
-                    if (posX >= xStart && posX <= xEnd && posY >= yStart && posY <= yEnd) {
-                        std::cout << x << "," << y << std::endl;
+                    if (posX >= startX && posX <= endX && posY >= startY && posY <= endY) {
                         sol++;
-                        wasSolution = true;
+                        solution = true;
                     }
                 }
             }
