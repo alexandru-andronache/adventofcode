f3f50ac32d46fc84316cf95481bd989b7adf88d7
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-17 06:31:18 +0100

    2021 - day 17

--- 2021/day17/main.cpp
+++ 2021/day17/main.cpp
@@ -1,14 +1,80 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
+#include <vector>
 
 namespace aoc2021_day17 {
     int part_1(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+        std::vector<std::string> tokens = utils::splitString(input, " : =.,");
+        int xStart = std::stoi(tokens[3].c_str());
+        int xEnd = std::stoi(tokens[4].c_str());
+        int yStart = std::stoi(tokens[6].c_str());
+        int yEnd = std::stoi(tokens[7].c_str());
+        int sol = std::numeric_limits<int>::min();
+
+        for (int x = -1000; x <= 1000; ++x) {
+            for (int y = -1000; y <= 1000; ++y) {
+                int posX = 0, posY = 0;
+                int speedX = x, speedY = y;
+                int maxY = std::numeric_limits<int>::min();
+                bool wasSolution = false;
+                while (posY >= yStart && !wasSolution) {
+                    posX += speedX;
+                    posY += speedY;
+                    maxY = std::max(maxY, posY);
+                    if (speedX < 0) {
+                        speedX++;
+                    }
+                    else if (speedX != 0) {
+                        speedX--;
+                    }
+                    speedY--;
+                    if (posX >= xStart && posX <= xEnd && posY >= yStart && posY <= yEnd) {
+                        wasSolution = true;
+                        sol = std::max(sol, maxY);
+                    }
+                }
+            }
+        }
+
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::string input = file::readFileAsString(path);
+        std::vector<std::string> tokens = utils::splitString(input, " : =.,");
+        int xStart = std::stoi(tokens[3].c_str());
+        int xEnd = std::stoi(tokens[4].c_str());
+        int yStart = std::stoi(tokens[6].c_str());
+        int yEnd = std::stoi(tokens[7].c_str());
+        int sol = 0;
+
+        for (int x = -1000; x <= 1000; ++x) {
+            for (int y = -1000; y <= 1000; ++y) {
+                int posX = 0, posY = 0;
+                int speedX = x, speedY = y;
+                bool wasSolution = false;
+                while (posY >= yStart && !wasSolution) {
+                    posX += speedX;
+                    posY += speedY;
+                    if (speedX < 0) {
+                        speedX++;
+                    }
+                    else if (speedX != 0) {
+                        speedX--;
+                    }
+                    speedY--;
+                    if (posX >= xStart && posX <= xEnd && posY >= yStart && posY <= yEnd) {
+                        std::cout << x << "," << y << std::endl;
+                        sol++;
+                        wasSolution = true;
+                    }
+                }
+            }
+        }
+
+        return sol;
     }
 }
 
--- 2021/day17/tests/TestClass.cpp
+++ 2021/day17/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day17{
     TEST_F(Tests2021Day17, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day17/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day17/input_test.in"), 45);
     }
 
     TEST_F(Tests2021Day17, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day17/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day17/input.in"), 8646);
     }
 
     TEST_F(Tests2021Day17, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day17/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day17/input_test.in"), 112);
     }
 
     TEST_F(Tests2021Day17, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day17/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day17/input.in"), 5945);
     }
 }
