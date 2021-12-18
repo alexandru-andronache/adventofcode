3041712056a942cce214852bc5e6fbd3b8d71c86
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-18 10:33:40 +0100

    2021 - day 18

--- 2021/day18/main.cpp
+++ 2021/day18/main.cpp
@@ -1,14 +1,172 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
+#include <list>
 
 namespace aoc2021_day18 {
+    const int OPEN = -1;
+    const int CLOSE = -2;
+
+    class Number {
+    public:
+        Number() {
+        }
+
+        void add(std::string line) {
+            bool needClose = false;
+            if (!state.empty()) {
+                state.push_front(OPEN);
+                needClose = true;
+            }
+            for (const auto& c : line) {
+                if (c == '[') {
+                    state.push_back(OPEN);
+                }
+                else if (c == ']') {
+                    state.push_back(CLOSE);
+                }
+                else if (c != ',') {
+                    state.push_back(c - '0');
+                }
+            }
+            if (needClose) {
+                state.push_back(CLOSE);
+            }
+        }
+
+        void split(std::list<int>::iterator it) {
+            int value = *it;
+            *it = OPEN;
+            std::advance(it, 1);
+            it = state.insert(it, value / 2);
+            std::advance(it, 1);
+            it = state.insert(it, value / 2 + value % 2);
+            std::advance(it, 1);
+            state.insert(it, CLOSE);
+        }
+
+        void explode(std::list<int>::iterator it) {
+            int value1 = *(std::next(it, 1));
+            int value2 = *(std::next(it, 2));
+
+            auto before = it;
+            while (before != state.begin() && *before < 0) {
+                std::advance(before, -1);
+            }
+            if (before != state.begin()) {
+                *before += value1;
+            }
+
+            *it = 0;
+            std::advance(it, 1);
+            auto end = it;
+            std::advance(end, 3);
+
+            auto after = end;
+            while (after != state.end() && *after < 0) {
+                std::advance(after, 1);
+            }
+            if (after != state.end()) {
+                *after += value2;
+            }
+
+            state.erase(it, end);
+        }
+
+        bool shouldSpit() {
+            auto it = state.begin();
+            while (it != state.end()) {
+                if (*it > 9) {
+                    split(it);
+                    return true;
+                }
+                std::advance(it, 1);
+            }
+            return false;
+        }
+
+        // applies the next operation
+        // return true if it's not finished
+        bool parse() {
+            auto it = state.begin();
+            int braces = 0;
+            while (it != state.end()) {
+                if (*it == OPEN) {
+                    braces++;
+                }
+                else if (*it == CLOSE) {
+                    braces--;
+                }
+                if (braces > 4) {
+                    int value1 = *(std::next(it, 1));
+                    int value2 = *(std::next(it, 2));
+                    if (value1 >= 0 && value2 >= 0) {
+                        explode(it);
+                        return true;
+                    }
+                }
+                std::advance(it, 1);
+            }
+            if (shouldSpit()) {
+                return true;
+            }
+            return false;
+        }
+
+        void minimize() {
+            auto it = state.begin();
+            while (*it != CLOSE) {
+                std::advance(it, 1);
+            }
+            int value1 = *(std::next(it, -1));
+            int value2 = *(std::next(it, -2));
+            int value = value1 * 2 + value2 * 3;
+            *it = value;
+            auto begin = it;
+            std::advance(begin, -3);
+            state.erase(begin, it);
+        }
+
+        int magnitude() {
+            while (state.size() > 1) {
+                minimize();
+            }
+            return state.front();
+        }
+
+    private:
+        std::list<int> state;
+    };
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        Number nr;
+        for (const auto& line : input) {
+            nr.add(line);
+            while (nr.parse()) {
+            }
+        }
+
+        return nr.magnitude();
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        int maxSol = 0;
+        for (int i = 0; i < input.size(); ++i) {
+            for (int j = 0; j < input.size(); ++j) {
+                if (i != j) {
+                    Number nr;
+                    nr.add(input[i]);
+                    nr.add(input[j]);
+                    while (nr.parse()) {
+                    }
+                    int magnitude = nr.magnitude();
+                    maxSol = std::max(maxSol, magnitude);
+                }
+            }
+        }
+        return maxSol;
     }
 }
 
--- 2021/day18/tests/TestClass.cpp
+++ 2021/day18/tests/TestClass.cpp
@@ -3,18 +3,26 @@
 
 namespace aoc2021_day18 {
     TEST_F(Tests2021Day18, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day18/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day18/input_test.in"), 1384);
+    }
+
+    TEST_F(Tests2021Day18, part_1_test_2) {
+        ASSERT_EQ(part_1("../2021/day18/input_test2.in"), 3488);
+    }
+
+    TEST_F(Tests2021Day18, part_1_test_3) {
+        ASSERT_EQ(part_1("../2021/day18/input_test3.in"), 4140);
     }
 
     TEST_F(Tests2021Day18, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day18/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day18/input.in"), 4433);
     }
 
     TEST_F(Tests2021Day18, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day18/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day18/input_test3.in"), 3993);
     }
 
     TEST_F(Tests2021Day18, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day18/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day18/input.in"), 4559);
     }
 }
