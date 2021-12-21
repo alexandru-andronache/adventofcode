2a7d370e0097090c62acfad7b324c05f79df5d1f
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-21 12:23:51 +0100

    2021 - day 20

--- 2021/day20/main.cpp
+++ 2021/day20/main.cpp
@@ -1,16 +1,74 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
-#include <cmath>
-#include <optional>
+#include <vector>
+#include <numeric>
 
 namespace aoc2021_day20 {
+    int solve(std::string_view path, int steps) {
+        const int N = 1000;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::vector<bool>> map(N, std::vector<bool>(N, false));
+
+        int middle = N / 2 - input[2].size() / 2;
+        utils::point start {middle, middle};
+        int size = input[2].size();
+
+        for (int i = 2; i < input.size(); ++i) {
+            for (int j = 0; j < input[i].size(); ++j) {
+                map[start.x + i - 2][start.y + j] = (input[i][j] == '#');
+            }
+        }
+
+        for (int step = 0; step < steps; ++step) {
+            int newSol = 0;
+            std::vector<std::vector<bool>> newMap(N, std::vector<bool>(N, false));
+            start.x -= 1;
+            start.y -= 1;
+            size += 2;
+            for (int x = start.x; x < start.x + size; ++x) {
+                for (int y = start.y; y < start.y + size; ++y) {
+                    std::bitset<9> mask;
+                    int k = 0;
+                    for (int i = -1; i <= 1; ++i) {
+                        for (int j = -1; j <= 1; ++j) {
+                            mask[8 - k] = map[x + i][y + j];
+                            k++;
+                        }
+                    }
+                    newMap[x][y] = input[0][mask.to_ulong()] == '#';
+                    if (newMap[x][y]) {
+                        newSol++;
+                    }
+                }
+            }
+
+            if (input[0][0] == '#') {
+                for (int i = 0; i < N; ++i) {
+                    for (int j = 0; j < N; ++j) {
+                        if (i < start.x || i >= start.x + size || j < start.y || j >= start.y + size) {
+                            newMap[i][j] = (step % 2 == 0);
+                        }
+                    }
+                }
+            }
+            map = newMap;
+        }
+
+        int sol = 0;
+        for (const auto& line : map) {
+            sol += std::count(line.begin(), line.end(), true);
+        }
+
+        return sol;
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        return solve(path, 2);
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        return solve(path, 50);
     }
 
 }
--- 2021/day20/tests/TestClass.cpp
+++ 2021/day20/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2021_day20 {
     TEST_F(Tests2021Day20, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day20/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day20/input_test.in"), 35);
     }
 
     TEST_F(Tests2021Day20, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day20/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day20/input.in"), 5573);
     }
 
     TEST_F(Tests2021Day20, part_2_test) {
-        ASSERT_EQ(part_2("../2021/day20/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2021/day20/input_test.in"), 3351);
     }
 
     TEST_F(Tests2021Day20, part_2_real_test) {
-        ASSERT_EQ(part_2("../2021/day20/input.in"), 0);
+        ASSERT_EQ(part_2("../2021/day20/input.in"), 20097);
     }
 }
