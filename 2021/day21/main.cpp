cd7ff14e17a5e941a10c0dac7711b8ed04968744
alexandru-andronache <al3x.andronache@gmail.com>
2021-12-21 09:20:26 +0100

    2021 - day 21

--- 2021/day21/main.cpp
+++ 2021/day21/main.cpp
@@ -4,34 +4,29 @@
 #include <map>
 
 namespace aoc2021_day21 {
+
+    void advanceDice(int& position, int& score, int& dice, int& rolls) {
+        for (int i = 1; i <= 3; ++i) {
+            dice++;
+            if (dice > 100) {
+                dice = 1;
+            }
+            position += dice;
+        }
+        rolls += 3;
+        position = position % 10;
+        if (position == 0) {
+            position = 10;
+        }
+        score += position;
+    }
+
     unsigned long long part_1(int startPos1, int startPos2) {
         int score1 = 0, score2 = 0, dice = 0, rolls = 0;
         while (score1 < 1000 && score2 < 1000) {
-            for (int i = 1; i <= 3; ++i) {
-                dice++;
-                if (dice > 100) {
-                    dice = 1;
-                }
-                startPos1 += dice;
-            }
-            rolls += 3;
-            while (startPos1 > 10) {
-                startPos1 -= 10;
-            }
-            score1 += startPos1;
+            advanceDice(startPos1, score1, dice, rolls);
             if (score1 < 1000) {
-                for (int i = 1; i <= 3; ++i) {
-                    dice++;
-                    if (dice > 100) {
-                        dice = 1;
-                    }
-                    startPos2 += dice;
-                }
-                rolls += 3;
-                while (startPos2 > 10) {
-                    startPos2 -= 10;
-                }
-                score2 += startPos2;
+                advanceDice(startPos2, score2, dice, rolls);
             }
         }
 
@@ -53,64 +48,53 @@ namespace aoc2021_day21 {
 
     std::map<State, std::pair<unsigned long long, unsigned long long>, decltype(comparator)> prevScores(comparator);
 
-    FILE *f = fopen("output.txt", "w");
+    std::vector<int> weight{0, 0, 0, 1, 3, 6, 7, 6, 3, 1};
 
     std::pair<unsigned long long, unsigned long long> step2(const int startPos1, const int startPos2, const int score1, const int score2, const int activePlayer) {
-        if (prevScores.find({startPos1, startPos2, score1, score2, activePlayer}) != prevScores.end()) {
-            return prevScores[{startPos1, startPos2, score1, score2, activePlayer}];
-        }
-
         unsigned long long winP1 = 0;
         unsigned long long winP2 = 0;
-//        int pp1 = startPos1;
-//        int pp2 = startPos2;
-//        int ps1 = score1;
-//        int ps2 = score2;
-//        int activePlayerT = activePlayer;
-        for (int dice1 = 1; dice1 <= 3; ++dice1) {
-            for (int dice2 = 1; dice2 <= 3; ++dice2) {
-                for (int dice3 = 1; dice3 <= 3; ++dice3) {
-                    if (activePlayer == 1) {
-                        int newStartPos1 = startPos1 + dice1 + dice2 + dice3;
-                        while (newStartPos1 > 10) {
-                            newStartPos1 -= 10;
-                        }
-                        int newScore1 = score1 + newStartPos1;
-                        if (newScore1 >= 21) {
-                            winP1++;
-                        }
-                        else {
-                            if (prevScores.find({newStartPos1, startPos2, newScore1, score2, 2}) == prevScores.end()) {
-                                auto r = step2(newStartPos1, startPos2, newScore1, score2, 2);
-                                prevScores.insert({{newStartPos1, startPos2, newScore1, score2, 2}, r});
-                            }
-                            auto result = prevScores[{newStartPos1, startPos2, newScore1, score2, 2}];
-                            winP1 += result.first;
-                            winP2 += result.second;
-                        }
+
+        for (int sumDice = 3; sumDice <= 9; ++sumDice) {
+            if (activePlayer == 1) {
+                int newStartPos1 = startPos1 + sumDice;
+                while (newStartPos1 > 10) {
+                    newStartPos1 -= 10;
+                }
+                int newScore1 = score1 + newStartPos1;
+                if (newScore1 >= 21) {
+                    winP1 += weight[sumDice];
+                }
+                else {
+                    if (prevScores.find({newStartPos1, startPos2, newScore1, score2, 2}) == prevScores.end()) {
+                        auto r = step2(newStartPos1, startPos2, newScore1, score2, 2);
+                        prevScores.insert({{newStartPos1, startPos2, newScore1, score2, 2}, r});
                     }
-                    else {
-                        int newStartPos2 = startPos2 + dice1 + dice2 + dice3;
-                        while (newStartPos2 > 10) {
-                            newStartPos2 -= 10;
-                        }
-                        int newScore2 = score2 + newStartPos2;
-                        if (newScore2 >= 21) {
-                            winP2++;
-                        }
-                        else {
-                            if (prevScores.find({startPos1, newStartPos2, score1, newScore2, 1}) == prevScores.end()) {
-                                auto r = step2(startPos1, newStartPos2, score1, newScore2, 1);
-                                prevScores.insert({{startPos1, newStartPos2, score1, newScore2, 1}, r});
-                            }
-                            auto result = prevScores[{startPos1, newStartPos2, score1, newScore2, 1}];
-                            winP1 += result.first;
-                            winP2 += result.second;
-                        }
+                    auto result = prevScores[{newStartPos1, startPos2, newScore1, score2, 2}];
+                    winP1 += result.first * weight[sumDice];
+                    winP2 += result.second * weight[sumDice];
+                }
+            }
+            else {
+                int newStartPos2 = startPos2 + sumDice;
+                while (newStartPos2 > 10) {
+                    newStartPos2 -= 10;
+                }
+                int newScore2 = score2 + newStartPos2;
+                if (newScore2 >= 21) {
+                    winP2 += weight[sumDice];
+                }
+                else {
+                    if (prevScores.find({startPos1, newStartPos2, score1, newScore2, 1}) == prevScores.end()) {
+                        auto r = step2(startPos1, newStartPos2, score1, newScore2, 1);
+                        prevScores.insert({{startPos1, newStartPos2, score1, newScore2, 1}, r});
                     }
+                    auto result = prevScores[{startPos1, newStartPos2, score1, newScore2, 1}];
+                    winP1 += result.first * weight[sumDice];
+                    winP2 += result.second * weight[sumDice];
                 }
             }
         }
+
         return {winP1, winP2};
     }
 
