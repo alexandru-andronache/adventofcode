9b5ffbdfc3aab2f29eb9b757508bc892ef670a3c
alexandru-andronache <al3x.andronache@gmail.com>
2022-01-21 19:52:27 +0100

    2021 - day 25

--- 2021/day25/main.cpp
+++ 2021/day25/main.cpp
@@ -1,10 +1,64 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
+#include <map>
 
 namespace aoc2021_day25 {
     int part_1(std::string_view path) {
-        return 0;
+        enum State{
+            EMPTY = 0,
+            LEFT = 1,
+            DOWN = 2
+        };
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        const int N = lines.size();
+        const int M = lines[0].size();
+        std::map<char, State> mapping {{'.', State::EMPTY}, {'>', State::LEFT}, {'v', State::DOWN}};
+
+        std::vector<std::vector<State>> map(N, std::vector<State>(M, State::EMPTY));
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                map[i][j] = mapping[lines[i][j]];
+            }
+        }
+
+        int steps = 0;
+        bool changed = true;
+
+        while (changed) {
+            changed = false;
+            std::vector<std::vector<State>> newMap(N, std::vector<State>(M, State::EMPTY));
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    if (map[i][j] == State::LEFT && map[i][(j + 1) % M] == State::EMPTY && newMap[i][(j + 1) % M] == State::EMPTY) {
+                        newMap[i][(j + 1) % M] = State::LEFT;
+                        newMap[i][j] = State::EMPTY;
+                        changed = true;
+                    }
+                    else if (newMap[i][j] == State::EMPTY) {
+                        newMap[i][j] = map[i][j];
+                    }
+                }
+            }
+            map = newMap;
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    if (map[i][j] == State::DOWN && map[(i + 1) % N][j] == State::EMPTY && newMap[(i + 1) % N][j] == State::EMPTY)
+                    {
+                        newMap[(i + 1) % N][j] = State::DOWN;
+                        newMap[i][j] = State::EMPTY;
+                        changed = true;
+                    }
+                    else if (newMap[i][j] == State::EMPTY) {
+                        newMap[i][j] = map[i][j];
+                    }
+                }
+            }
+            map = newMap;
+            steps++;
+        }
+
+        return steps;
     }
 }
 
--- 2021/day25/tests/TestClass.cpp
+++ 2021/day25/tests/TestClass.cpp
@@ -3,10 +3,10 @@
 
 namespace aoc2021_day25 {
     TEST_F(Tests2021Day25, part_1_test) {
-        ASSERT_EQ(part_1("../2021/day25/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2021/day25/input_test.in"), 58);
     }
 
     TEST_F(Tests2021Day25, part_1_real_test) {
-        ASSERT_EQ(part_1("../2021/day25/input.in"), 0);
+        ASSERT_EQ(part_1("../2021/day25/input.in"), 489);
     }
 }
