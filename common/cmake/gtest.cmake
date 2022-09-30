dcee273fc4a0105b992895d310d931fb72606263
alex <alexandru.andronache@icloud.com>
2022-09-30 13:40:48 +0200

    test refactor cmake

--- common/cmake/gtest.cmake
+++ common/cmake/gtest.cmake
@@ -5,11 +5,12 @@ ExternalProject_Add(
         GIT_REPOSITORY https://github.com/google/googletest
         GIT_TAG release-1.11.0
         TIMEOUT 10
+        SOURCE_DIR  ${CMAKE_BINARY_DIR}/third_party/googletest/googletest
         # Force separate output paths for debug and release builds to allow easy
         # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
-        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
+        # -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
+        # -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
         -Dgtest_force_shared_crt=ON
         # Disable install step
         INSTALL_COMMAND ""
@@ -22,7 +23,9 @@ ExternalProject_Add(
             -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER})
 
 # Specify include dir
-ExternalProject_Get_Property(googletest_utilities source_dir)
+# ExternalProject_Get_Property(googletest_utilities source_dir)
 
-include_directories(${source_dir}/googletest/include)
-include_directories(${source_dir}/googlemock/include)
+include_directories(${CMAKE_BINARY_DIR}/third_party/googletest/googletest/googletest/include)
+include_directories(${CMAKE_BINARY_DIR}/third_party/googletest/googletest/googlemock/include)
+
+link_directories(${CMAKE_BINARY_DIR}/third_party/src/googletest_utilities-build/lib)
