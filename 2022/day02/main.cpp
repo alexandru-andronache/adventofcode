c654cbd019722aeb6e7fdd508c0e98098415e5b9
alexandru.andronache <alexandru@king.com>
2022-12-02 06:46:08 +0100

    2022 - day 2

--- 2022/day02/main.cpp
+++ 2022/day02/main.cpp
@@ -1,13 +1,60 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <map>
 
 namespace aoc2022_day02 {
+    const int ROCK = 1;
+    const int PAPER = 2;
+    const int SCISSORS = 3;
+    const int LOST = 0;
+    const int DRAW = 3;
+    const int WIN = 6;
+    std::map<std::string, int> shapes {{"A", ROCK}, {"B", PAPER}, {"C", SCISSORS},
+                                       {"X", ROCK}, {"Y", PAPER}, {"Z", SCISSORS}};
+    std::map<std::string, int> results{{"X", LOST}, {"Y", DRAW}, {"Z", WIN}};
+
+    std::map<std::pair<std::string, std::string>, int> outcome_part_1 {{{"A", "X"}, DRAW},
+                                                                       {{"A", "Y"}, WIN},
+                                                                       {{"A", "Z"}, LOST},
+                                                                       {{"B", "X"}, LOST},
+                                                                       {{"B", "Y"}, DRAW},
+                                                                       {{"B", "Z"}, WIN},
+                                                                       {{"C", "X"}, WIN},
+                                                                       {{"C", "Y"}, LOST},
+                                                                       {{"C", "Z"}, DRAW}};
+
+    std::map<std::pair<std::string, int>, std::string> outcome_part_2 {{{"A", DRAW}, "X"},
+                                                                       {{"A", LOST}, "Z"},
+                                                                       {{"A", WIN}, "Y"},
+                                                                       {{"B", DRAW}, "Y"},
+                                                                       {{"B", LOST}, "X"},
+                                                                       {{"B", WIN}, "Z"},
+                                                                       {{"C", DRAW}, "Z"},
+                                                                       {{"C", LOST}, "Y"},
+                                                                       {{"C", WIN}, "X"}};
+
     int part_1(std::string_view path) {
-        return 0;
+        int sol = 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        for (const auto& line : input) {
+            std::vector<std::string> tokens = utils::splitString(line, " ");
+            sol += outcome_part_1[{tokens[0], tokens[1]}] + shapes[tokens[1]];
+        }
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        int sol = 0;
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        for (const auto& line : input) {
+            std::vector<std::string> tokens = utils::splitString(line, " ");
+
+            sol += results[tokens[1]] + shapes[outcome_part_2[{tokens[0], results[tokens[1]]}]];
+        }
+        return sol;
     }
 }
 
--- 2022/day02/tests/TestClass.cpp
+++ 2022/day02/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2022_day02 {
     TEST_F(Tests2022Day02, part_1_test) {
-        ASSERT_EQ(part_1("../2022/day02/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2022/day02/input_test.in"), 15);
     }
 
     TEST_F(Tests2022Day02, part_1_real_test) {
-        ASSERT_EQ(part_1("../2022/day02/input.in"), 0);
+        ASSERT_EQ(part_1("../2022/day02/input.in"), 11841);
     }
 
     TEST_F(Tests2022Day02, part_2_test) {
-        ASSERT_EQ(part_2("../2022/day02/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2022/day02/input_test.in"), 12);
     }
 
-    TEST_F(Tests2022Day02, part_2_real_test) {
-        ASSERT_EQ(part_2("../2022/day02/input.in"), 0);
-    }
+//    TEST_F(Tests2022Day02, part_2_real_test) {
+//        ASSERT_EQ(part_2("../2022/day02/input.in"), 13022);
+//    }
 }
