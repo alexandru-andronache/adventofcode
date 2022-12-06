44b3a07ffb3cd4de2526e0590693f67b9c1d0b81
alexandru.andronache <alexandru@king.com>
2022-12-06 06:34:45 +0100

    2022 - day 6

--- 2022/day06/main.cpp
+++ 2022/day06/main.cpp
@@ -1,14 +1,38 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 namespace aoc2022_day06 {
-    int part_1(std::string_view path) {
+    int solve(std::string_view path, int size) {
+        std::string input = file::readFileAsString(path);
+
+        int i = 0;
+        std::vector<char> t;
+        while (i < input.size()) {
+            auto it = std::find(t.begin(), t.end(), input[i]);
+            if (it != t.end()) {
+                std::vector<char> tmp;
+                std::copy(std::next(it), t.end(), std::back_inserter(tmp));
+                t.clear();
+                std::copy(tmp.begin(), tmp.end(), std::back_inserter(t));
+            }
+            t.push_back(input[i]);
+            if (t.size() == size) {
+                return i + 1;
+            }
+            i++;
+        }
+
         return 0;
+    }
+    int part_1(std::string_view path) {
+        return solve(path, 4);
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        return solve(path, 14);
     }
 }
 
--- 2022/day06/tests/TestClass.cpp
+++ 2022/day06/tests/TestClass.cpp
@@ -3,18 +3,42 @@
 
 namespace aoc2022_day06 {
     TEST_F(Tests2022Day06, part_1_test) {
-        ASSERT_EQ(part_1("../2022/day06/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2022/day06/input_test.in"), 5);
+    }
+
+    TEST_F(Tests2022Day06, part_1_test_2) {
+        ASSERT_EQ(part_1("../2022/day06/input_test2.in"), 6);
+    }
+
+    TEST_F(Tests2022Day06, part_1_test_3) {
+        ASSERT_EQ(part_1("../2022/day06/input_test3.in"), 10);
+    }
+
+    TEST_F(Tests2022Day06, part_1_test_4) {
+        ASSERT_EQ(part_1("../2022/day06/input_test4.in"), 11);
     }
 
     TEST_F(Tests2022Day06, part_1_real_test) {
-        ASSERT_EQ(part_1("../2022/day06/input.in"), 0);
+        ASSERT_EQ(part_1("../2022/day06/input.in"), 1892);
     }
 
     TEST_F(Tests2022Day06, part_2_test) {
-        ASSERT_EQ(part_2("../2022/day06/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2022/day06/input_test.in"), 23);
+    }
+
+    TEST_F(Tests2022Day06, part_2_test_2) {
+        ASSERT_EQ(part_2("../2022/day06/input_test2.in"), 23);
+    }
+
+    TEST_F(Tests2022Day06, part_2_test_3) {
+        ASSERT_EQ(part_2("../2022/day06/input_test3.in"), 29);
+    }
+
+    TEST_F(Tests2022Day06, part_2_test_4) {
+        ASSERT_EQ(part_2("../2022/day06/input_test4.in"), 26);
     }
 
     TEST_F(Tests2022Day06, part_2_real_test) {
-        ASSERT_EQ(part_2("../2022/day06/input.in"), 0);
+        ASSERT_EQ(part_2("../2022/day06/input.in"), 2313);
     }
 }
