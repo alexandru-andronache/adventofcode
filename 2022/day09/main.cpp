c755971fccc8fc94c034e19cd46be394a325063f
alexandru.andronache <alexandru@king.com>
2022-12-09 09:54:34 +0100

    2022 - day 9

--- 2022/day09/main.cpp
+++ 2022/day09/main.cpp
@@ -1,13 +1,59 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <numeric>
+#include <map>
 
 namespace aoc2022_day09 {
+    int sgn(int val) {
+        return (val > 0) - (val < 0);
+    }
+    void updateTail(std::vector<utils::point>& tail) {
+        for (auto it = std::next(tail.begin()); it != tail.end(); ++it) {
+            if (std::abs(it->x - std::prev(it)->x) > 1 || std::abs(it->y - std::prev(it)->y) > 1) {
+                it->y += sgn(std::prev(it)->y - it->y);
+                it->x += sgn(std::prev(it)->x - it->x);
+            }
+        }
+    }
+
+    int solve(std::string_view path, int size) {
+        const int N = 2000;
+        std::vector<std::vector<bool>> prev(N, std::vector<bool>(N, false));
+        std::map<std::string, utils::point> dir {{"R", {0, 1}},
+                                                 {"L", {0, -1}},
+                                                 {"U", {-1, 0}},
+                                                 {"D", {1, 0}}};
+
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<utils::point> tail {};
+        prev[N / 2][N / 2] = true;
+        for (int i = 0; i < size; ++i) {
+            tail.push_back({N / 2, N / 2});
+        }
+        for (const auto& line : input) {
+            std::vector<std::string> t = utils::splitString(line, " ");
+            int steps = std::stoi(t[1]);
+
+            for (int i = 0; i < steps; ++i) {
+                tail[0].x += dir[t[0]].x;
+                tail[0].y += dir[t[0]].y;
+                updateTail(tail);
+                prev[tail.back().x][tail.back().y] = true;
+            }
+        }
+
+        return std::accumulate(prev.cbegin(), prev.cend(), 0, [](auto lhs, const auto& rhs) {
+            return std::accumulate(rhs.cbegin(), rhs.cend(), lhs);
+        });
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        return solve(path, 2);
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        return solve(path, 10);
     }
 }
 
--- 2022/day09/tests/TestClass.cpp
+++ 2022/day09/tests/TestClass.cpp
@@ -3,18 +3,22 @@
 
 namespace aoc2022_day09 {
     TEST_F(Tests2022Day09, part_1_test) {
-        ASSERT_EQ(part_1("../2022/day09/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2022/day09/input_test.in"), 13);
     }
 
     TEST_F(Tests2022Day09, part_1_real_test) {
-        ASSERT_EQ(part_1("../2022/day09/input.in"), 0);
+        ASSERT_EQ(part_1("../2022/day09/input.in"), 6314);
     }
 
     TEST_F(Tests2022Day09, part_2_test) {
-        ASSERT_EQ(part_2("../2022/day09/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2022/day09/input_test.in"), 1);
+    }
+
+    TEST_F(Tests2022Day09, part_2_test_2) {
+        ASSERT_EQ(part_2("../2022/day09/input_test2.in"), 36);
     }
 
     TEST_F(Tests2022Day09, part_2_real_test) {
-        ASSERT_EQ(part_2("../2022/day09/input.in"), 0);
+        ASSERT_EQ(part_2("../2022/day09/input.in"), 2504);
     }
 }
