af536f630f515a892ec5776e0d584de2694490fb
alexandru.andronache <alexandru@king.com>
2022-12-12 07:18:42 +0100

    2022 - day 12

--- 2022/day12/main.cpp
+++ 2022/day12/main.cpp
@@ -1,15 +1,94 @@
 #include "file.h"
 #include "utilities.h"
+#include "direction.h"
 #include <iostream>
 #include <set>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <limits>
 
 namespace aoc2022_day12 {
+    int solve(const std::vector<std::string>& in, int sx, int sy, int ex, int ey) {
+        std::vector<std::vector<int>> p(in.size(), std::vector<int>(in[0].size(), 0));
+        direction::Direction dir;
+
+        std::vector<std::pair<int, int>> nr;
+        nr.push_back({sx, sy});
+        while (nr.size() > 0) {
+            std::vector<std::pair<int, int>> t;
+            for (const auto& n : nr) {
+                for (const auto& d : dir.directions) {
+                    if (n.first + d.x >= 0 && n.first + d.x < in.size() && n.second + d.y >= 0 && n.second + d.y < in[0].size()) {
+                        if (in[n.first + d.x][n.second + d.y] - in[n.first][n.second] <= 1) {
+                            if (p[n.first + d.x][n.second + d.y] == 0 ||
+                                p[n.first + d.x][n.second + d.y] > p[n.first][n.second] + 1) {
+                                p[n.first + d.x][n.second + d.y] = p[n.first][n.second] + 1;
+                                t.emplace_back(n.first + d.x, n.second + d.y);
+                            }
+                        }
+                    }
+                }
+            }
+            nr = t;
+        }
+
+        return p[ex][ey];
+    }
+
     int part_1(std::string_view path) {
-        return 0;
+        std::vector<std::string> in = file::readFileAsArrayString(path);
+
+        int sX = 0, sY = 0, ex, ey = 0;
+        for (int i = 0; i < in.size(); ++i) {
+            for (int j = 0; j < in[0].size(); ++j) {
+                if (in[i][j] == 'S') {
+                    sX = i;
+                    sY = j;
+                }
+                else if (in[i][j] == 'E') {
+                    ex = i;
+                    ey = j;
+                }
+            }
+        }
+        in[sX][sY] = 'a';
+        in[ex][ey] = 'z';
+
+        return solve(in, sX, sY, ex, ey);
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> in = file::readFileAsArrayString(path);
+
+        int sX = 0, sY = 0, ex, ey = 0;
+        for (int i = 0; i < in.size(); ++i) {
+            for (int j = 0; j < in[0].size(); ++j) {
+                if (in[i][j] == 'S') {
+                    sX = i;
+                    sY = j;
+                }
+                else if (in[i][j] == 'E') {
+                    ex = i;
+                    ey = j;
+                }
+            }
+        }
+        in[sX][sY] = 'a';
+        in[ex][ey] = 'z';
+
+        int min = 10000;
+        for (int i = 0; i < in.size(); ++i) {
+            for (int j = 0; j < in[0].size(); ++j) {
+                if (in[i][j] == 'a') {
+                    int x = solve(in, i, j, ex, ey);
+                    if (x > 0)
+                        min = std::min(min, x);
+                }
+            }
+        }
+
+        return min;
     }
 }
 
--- 2022/day12/tests/TestClass.cpp
+++ 2022/day12/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2022_day12 {
     TEST_F(Tests2022Day12, part_1_test) {
-        ASSERT_EQ(part_1("../2022/day12/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2022/day12/input_test.in"), 31);
     }
 
     TEST_F(Tests2022Day12, part_1_real_test) {
-        ASSERT_EQ(part_1("../2022/day12/input.in"), 0);
+        ASSERT_EQ(part_1("../2022/day12/input.in"), 425);
     }
 
     TEST_F(Tests2022Day12, part_2_test) {
-        ASSERT_EQ(part_2("../2022/day12/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2022/day12/input_test.in"), 29);
     }
 
     TEST_F(Tests2022Day12, part_2_real_test) {
-        ASSERT_EQ(part_2("../2022/day12/input.in"), 0);
+        ASSERT_EQ(part_2("../2022/day12/input.in"), 418);
     }
 }
