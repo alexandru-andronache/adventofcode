6ac891446a2812c12ebb3fc6c00bde23d9ae2429
alexandru.andronache <alexandru@king.com>
2022-12-21 22:13:53 +0100

    2022 - day 21

--- 2022/day21/main.cpp
+++ 2022/day21/main.cpp
@@ -1,14 +1,133 @@
 #include "file.h"
 #include "utilities.h"
 #include <iostream>
+#include <map>
 
 namespace aoc2022_day21 {
-    int part_1(std::string_view path) {
+    struct monkey {
+        std::string op1;
+        std::string op2;
+        std::string operation;
+        long long val;
+        bool isCalculated;
+        bool isHumanDependent;
+    };
+
+    std::map<std::string, monkey> monkeys;
+
+    long long calculate(std::string m) {
+        if (m == "humn") {
+            monkeys[m].isHumanDependent = true;
+        }
+        if (monkeys[m].isCalculated) {
+            return monkeys[m].val;
+        }
+        monkeys[monkeys[m].op1].val = calculate(monkeys[m].op1);
+        if (monkeys[monkeys[m].op1].isHumanDependent) {
+            monkeys[m].isHumanDependent = true;
+        }
+        monkeys[monkeys[m].op2].val = calculate(monkeys[m].op2);
+        if (monkeys[monkeys[m].op2].isHumanDependent) {
+            monkeys[m].isHumanDependent = true;
+        }
+        if (monkeys[m].operation == "+") {
+            return monkeys[monkeys[m].op1].val + monkeys[monkeys[m].op2].val;
+        }
+        if (monkeys[m].operation == "-") {
+            return monkeys[monkeys[m].op1].val - monkeys[monkeys[m].op2].val;
+        }
+        if (monkeys[m].operation == "*") {
+            return monkeys[monkeys[m].op1].val * monkeys[monkeys[m].op2].val;
+        }
+        if (monkeys[m].operation == "/") {
+            return monkeys[monkeys[m].op1].val / monkeys[monkeys[m].op2].val;
+        }
         return 0;
     }
 
-    int part_2(std::string_view path) {
+    long long calculateReverse(std::string m, long long totalValue) {
+        if (m == "humn") {
+            return totalValue;
+        }
+        if (monkeys[m].isCalculated) {
+            return monkeys[m].val;
+        }
+        if (monkeys[monkeys[m].op1].isHumanDependent) {
+            long long result = calculate(monkeys[m].op2);
+            if (monkeys[m].operation == "+") {
+                return calculateReverse(monkeys[m].op1, totalValue - result);
+            }
+            if (monkeys[m].operation == "-") {
+                return calculateReverse(monkeys[m].op1, totalValue + result);
+            }
+            if (monkeys[m].operation == "*") {
+                return calculateReverse(monkeys[m].op1, totalValue / result);
+            }
+            if (monkeys[m].operation == "/") {
+                return calculateReverse(monkeys[m].op1, totalValue * result);
+            }
+        }
+
+        if (monkeys[monkeys[m].op2].isHumanDependent) {
+            long long result = calculate(monkeys[m].op1);
+            if (monkeys[m].operation == "+") {
+                return calculateReverse(monkeys[m].op2, totalValue - result);
+            }
+            if (monkeys[m].operation == "-") {
+                return calculateReverse(monkeys[m].op2, result - totalValue);
+            }
+            if (monkeys[m].operation == "*") {
+                return calculateReverse(monkeys[m].op2, totalValue / result);
+            }
+            if (monkeys[m].operation == "/") {
+                return calculateReverse(monkeys[m].op2, result / totalValue);
+            }
+        }
+
         return 0;
+    }
+
+
+
+    long long part_1(std::string_view path) {
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        monkeys.clear();
+
+        for (const auto& l : lines) {
+            std::vector<std::string> t = utils::splitString(l, " :");
+            if (t.size() > 2) {
+                monkeys.insert({t[0], {t[1], t[3], t[2], 0, false, false}});
+            }
+            else {
+                monkeys.insert({t[0], {"", "", "", std::stoll(t[1]), true, false}});
+            }
+        }
+
+        return calculate("root");
+    }
+
+    long long part_2(std::string_view path) {
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        monkeys.clear();
+
+        for (const auto& l : lines) {
+            std::vector<std::string> t = utils::splitString(l, " :");
+            if (t.size() > 2) {
+                monkeys.insert({t[0], {t[1], t[3], t[2], 0, false, false}});
+            }
+            else {
+                monkeys.insert({t[0], {"", "", "", std::stoll(t[1]), true, false}});
+            }
+        }
+
+        calculate("root");
+
+        if (monkeys[monkeys["root"].op1].isHumanDependent) {
+            return calculateReverse(monkeys["root"].op1, monkeys[monkeys["root"].op2].val);
+        }
+        else {
+            return calculateReverse(monkeys["root"].op2, monkeys[monkeys["root"].op1].val);
+        }
     }
 }
 
--- 2022/day21/tests/TestClass.cpp
+++ 2022/day21/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2022_day21 {
     TEST_F(Tests2022Day21, part_1_test) {
-        ASSERT_EQ(part_1("../2022/day21/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2022/day21/input_test.in"), 152);
     }
 
     TEST_F(Tests2022Day21, part_1_real_test) {
-        ASSERT_EQ(part_1("../2022/day21/input.in"), 0);
+        ASSERT_EQ(part_1("../2022/day21/input.in"), 87457751482938);
     }
 
     TEST_F(Tests2022Day21, part_2_test) {
-        ASSERT_EQ(part_2("../2022/day21/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2022/day21/input_test.in"), 301);
     }
 
     TEST_F(Tests2022Day21, part_2_real_test) {
-        ASSERT_EQ(part_2("../2022/day21/input.in"), 0);
+        ASSERT_EQ(part_2("../2022/day21/input.in"), 3221245824363);
     }
 }
