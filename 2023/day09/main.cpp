0aa8a5211f914250e17b5ddba4fe96d4f46d6a89
alexandru.andronache <alexandru@king.com>
2023-12-09 06:37:40 +0100

    day 9

--- 2023/day09/main.cpp
+++ 2023/day09/main.cpp
@@ -1,13 +1,55 @@
 #include "file.h"
+#include "utilities.h"
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
 
 namespace aoc2023_day09 {
-    int part_1(std::string_view path) {
-        return 0;
+    unsigned long long part_1(std::string_view path) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        unsigned long long sol = 0;
+
+        for (const auto& line : input) {
+            std::vector<int> t = utils::splitStringToInt(line, " ");
+            bool isZero = false;
+            while (!t.empty() && !isZero) {
+                sol += t.back();
+                std::vector<int> t1;
+                for (int i = 0; i < t.size() - 1; ++i) {
+                    t1.push_back(t[i + 1] - t[i]);
+                }
+                t = t1;
+                isZero = std::all_of(t.begin(), t.end(), [](int i) {return i == 0;});
+            }
+        }
+        return sol;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long part_2(std::string_view path) {
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        unsigned long long sol = 0;
+
+        for (const auto& line : input) {
+            std::vector<int> t = utils::splitStringToInt(line, " ");
+            bool isZero = false;
+            std::vector<int> prev;
+            while (!t.empty() && !isZero) {
+                std::vector<int> t1;
+                prev.push_back(t.front());
+                for (int i = 0; i < t.size() - 1; ++i) {
+                    t1.push_back(t[i + 1] - t[i]);
+                }
+                t = t1;
+                isZero = std::all_of(t.begin(), t.end(), [](int i) {return i == 0;});
+            }
+            int p = 0;
+            for (int i = prev.size() - 1; i > 0; i--) {
+                p = prev[i] - p;
+            }
+            sol += prev[0] - p;
+        }
+        return sol;
     }
 }
 
--- 2023/day09/tests/TestClass.cpp
+++ 2023/day09/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2023_day09 {
     TEST_F(Tests2023Day09, part_1_test) {
-        ASSERT_EQ(part_1("../2023/day09/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2023/day09/input_test.in"), 114);
     }
 
     TEST_F(Tests2023Day09, part_1_real_test) {
-        ASSERT_EQ(part_1("../2023/day09/input.in"), 0);
+        ASSERT_EQ(part_1("../2023/day09/input.in"), 1702218515);
     }
 
     TEST_F(Tests2023Day09, part_2_test) {
-        ASSERT_EQ(part_2("../2023/day09/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2023/day09/input_test.in"), 2);
     }
 
     TEST_F(Tests2023Day09, part_2_real_test) {
-        ASSERT_EQ(part_2("../2023/day09/input.in"), 0);
+        ASSERT_EQ(part_2("../2023/day09/input.in"), 925);
     }
 }
