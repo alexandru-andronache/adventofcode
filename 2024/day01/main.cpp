53f2ad5fe97790d36e5f0859d9c046712855598f
Alexandru Andronache <Alexandru@king.com>
2024-12-01 06:17:24 +0100

    2024 day 01

--- 2024/day01/main.cpp
+++ 2024/day01/main.cpp
@@ -4,11 +4,41 @@
 
 namespace aoc2024_day01 {
     int part_1(std::string_view path) {
-        return 0;
+        int sol = 0;
+
+        std::vector<int> numbers = file::readFileAsArrayInt(path);
+        std::vector<int> numbers1;
+        std::vector<int> numbers2;
+        for (int i = 0; i < numbers.size(); i += 2) {
+            numbers1.push_back(numbers[i]);
+            numbers2.push_back(numbers[i + 1]);
+        }
+
+        std::ranges::sort(numbers1);
+        std::ranges::sort(numbers2);
+
+        for (int i = 0; i < numbers1.size(); ++i) {
+            sol += std::abs(numbers1[i] - numbers2[i]);
+        }
+
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        int sol = 0;
+
+        std::vector<int> numbers = file::readFileAsArrayInt(path);
+        std::vector<int> numbers1;
+        std::vector<int> numbers2;
+        for (int i = 0; i < numbers.size(); i += 2) {
+            numbers1.push_back(numbers[i]);
+            numbers2.push_back(numbers[i + 1]);
+        }
+
+        for (int n : numbers1) {
+            sol += n * std::ranges::count(numbers2, n);
+        }
+        return sol;
     }
 }
 
--- 2024/day01/tests/TestClass.cpp
+++ 2024/day01/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2024_day01 {
     TEST_F(Tests2024Day01, part_1_test) {
-        ASSERT_EQ(part_1("../2024/day01/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2024/day01/input_test.in"), 11);
     }
 
     TEST_F(Tests2024Day01, part_1_real_test) {
-        ASSERT_EQ(part_1("../2024/day01/input.in"), 0);
+        ASSERT_EQ(part_1("../2024/day01/input.in"), 1151792);
     }
 
     TEST_F(Tests2024Day01, part_2_test) {
-        ASSERT_EQ(part_2("../2024/day01/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2024/day01/input_test.in"), 31);
     }
 
     TEST_F(Tests2024Day01, part_2_real_test) {
-        ASSERT_EQ(part_2("../2024/day01/input.in"), 0);
+        ASSERT_EQ(part_2("../2024/day01/input.in"), 21790168);
     }
 }
