075ef86a80e2c0d3ca412bd51de809faf0a42a32
Alexandru Andronache <Alexandru@king.com>
2024-12-03 07:06:31 +0100

    2024 day 3

--- 2024/day03/main.cpp
+++ 2024/day03/main.cpp
@@ -2,12 +2,138 @@
 #include <iostream>
 
 namespace aoc2024_day03 {
-    int part_1(std::string_view path) {
-        return 0;
+    bool isSubStringNext(const std::string_view& str, int position, const std::string_view& substr) {
+        return str.substr(position, substr.size()) == substr;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long part_1(std::string_view path) {
+        unsigned long long sol = 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+
+        for (const auto& line : lines) {
+            int i = 0;
+            int nr1 = 0, nr2 = 0;
+            while (i < line.size()) {
+                nr1 = 0, nr2 = 0;
+                if (isSubStringNext(line, i, "mul")) {
+                    i += 3;
+                }
+                else {
+                    i++;
+                    continue;
+                }
+                if (i < line.size() && line[i] == '(') {
+                    i++;
+                    while (i < line.size() && line[i] >= '0' && line[i] <= '9') {
+                        nr1 = nr1 * 10 + (line[i] - '0');
+                        i++;
+                    }
+                }
+                else {
+                    i++;
+                    continue;
+                }
+                if (i < line.size() && line[i] == ',') {
+                    i++;
+                    while (i < line.size() && line[i] >= '0' && line[i] <= '9') {
+                        nr2 = nr2 * 10 + (line[i] - '0');
+                        i++;
+                    }
+                }
+                else {
+                    i++;
+                    continue;
+                }
+                if (i < line.size() && line[i] == ')') {
+                    i++;
+                }
+                else {
+                    i++;
+                    continue;
+                }
+                sol += nr1 * nr2;
+            }
+        }
+
+
+        return sol;
+    }
+
+    unsigned long long part_2(std::string_view path) {
+        unsigned long long sol = 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        bool enable = true;
+
+        for (const auto& line : lines) {
+            int i = 0;
+            int nr1 = 0, nr2 = 0;
+            while (i < line.size())
+            {
+                nr1 = 0, nr2 = 0;
+                if (isSubStringNext(line, i, "do()"))
+                {
+                    enable = true;
+                    i += 4;
+                    continue;
+                }
+                if (isSubStringNext(line, i, "don't()"))
+                {
+                    enable = false;
+                    i += 7;
+                    continue;
+                }
+                if (isSubStringNext(line, i, "mul"))
+                {
+                    i += 3;
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+                if (i < line.size() && line[i] == '(')
+                {
+                    i++;
+                    while (i < line.size() && line[i] >= '0' && line[i] <= '9') {
+                        nr1 = nr1 * 10 + (line[i] - '0');
+                        i++;
+                    }
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+                if (i < line.size() && line[i] == ',') {
+                    i++;
+                    while (i < line.size() && line[i] >= '0' && line[i] <= '9') {
+                        nr2 = nr2 * 10 + (line[i] - '0');
+                        i++;
+                    }
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+                if (i < line.size() && line[i] == ')') {
+                    i++;
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+
+                if (enable)
+                {
+                    sol += nr1 * nr2;
+                }
+            }
+        }
+
+
+        return sol;
     }
 }
 
--- 2024/day03/tests/TestClass.cpp
+++ 2024/day03/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2024_day03 {
     TEST_F(Tests2024Day03, part_1_test) {
-        ASSERT_EQ(part_1("../2024/day03/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2024/day03/input_test.in"), 161);
     }
 
     TEST_F(Tests2024Day03, part_1_real_test) {
-        ASSERT_EQ(part_1("../2024/day03/input.in"), 0);
+        ASSERT_EQ(part_1("../2024/day03/input.in"), 182619815);
     }
 
     TEST_F(Tests2024Day03, part_2_test) {
-        ASSERT_EQ(part_2("../2024/day03/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2024/day03/input_test2.in"), 48);
     }
 
     TEST_F(Tests2024Day03, part_2_real_test) {
-        ASSERT_EQ(part_2("../2024/day03/input.in"), 0);
+        ASSERT_EQ(part_2("../2024/day03/input.in"), 80747545);
     }
 }
