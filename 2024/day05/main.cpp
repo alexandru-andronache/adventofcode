4f04c538dee43417f0c87deddd7711c61b74f53b
Alexandru Andronache <Alexandru@king.com>
2024-12-05 06:13:06 +0100

    2024 day 5; i should make it nicer but i'm sleepy

--- 2024/day05/main.cpp
+++ 2024/day05/main.cpp
@@ -1,13 +1,84 @@
 #include "file.h"
 #include <iostream>
+#include <utilities.h>
 
 namespace aoc2024_day05 {
     int part_1(std::string_view path) {
-        return 0;
+        int sol = 0;
+        // std::string input = file::readFileAsString(path);
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::pair<int, int>> pages;
+        int i = 0;
+        while (input[i] != "") {
+            std::vector<int> p = utils::splitStringToInt(input[i], "|");
+            pages.emplace_back(p[0], p[1]);
+            i++;
+        }
+        i++;
+        while (i < input.size())
+        {
+            std::vector<int> p = utils::splitStringToInt(input[i], ",");
+            bool found = true;
+            for (int j = 0; j < p.size() - 1; ++j) {
+                for (const auto& page : pages) {
+                    if (p[j] == page.second && p[j + 1] == page.first) {
+                        found = false;
+                    }
+                }
+            }
+            if (found) {
+                sol += p[p.size() / 2];
+            }
+            i++;
+        }
+
+        return sol;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        int sol = 0;
+        // std::string input = file::readFileAsString(path);
+        std::vector<std::string> input = file::readFileAsArrayString(path);
+        std::vector<std::pair<int, int>> pages;
+        int i = 0;
+        while (input[i] != "") {
+            std::vector<int> p = utils::splitStringToInt(input[i], "|");
+            pages.emplace_back(p[0], p[1]);
+            i++;
+        }
+        i++;
+        while (i < input.size())
+        {
+            std::vector<int> p = utils::splitStringToInt(input[i], ",");
+            bool found = true;
+            for (int j = 0; j < p.size() - 1; ++j) {
+                for (const auto& page : pages) {
+                    if (p[j] == page.second && p[j + 1] == page.first) {
+                        found = false;
+                    }
+                }
+            }
+            if (!found) {
+                bool newFound = false;
+                while (!newFound)
+                {
+                    newFound = true;
+                    for (int j = 0; j < p.size() - 1; ++j) {
+                        for (const auto& page : pages) {
+                            if (p[j] == page.second && p[j + 1] == page.first) {
+                                newFound = false;
+                                std::swap(p[j], p[j + 1]);
+                                break;
+                            }
+                        }
+                    }
+                }
+                sol += p[p.size() / 2];
+            }
+            i++;
+        }
+
+        return sol;
     }
 }
 
--- 2024/day05/tests/TestClass.cpp
+++ 2024/day05/tests/TestClass.cpp
@@ -3,15 +3,15 @@
 
 namespace aoc2024_day05 {
     TEST_F(Tests2024Day05, part_1_test) {
-        ASSERT_EQ(part_1("../2024/day05/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2024/day05/input_test.in"), 143);
     }
 
     TEST_F(Tests2024Day05, part_1_real_test) {
-        ASSERT_EQ(part_1("../2024/day05/input.in"), 0);
+        ASSERT_EQ(part_1("../2024/day05/input.in"), 7024);
     }
 
     TEST_F(Tests2024Day05, part_2_test) {
-        ASSERT_EQ(part_2("../2024/day05/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2024/day05/input_test.in"), 123);
     }
 
     TEST_F(Tests2024Day05, part_2_real_test) {
