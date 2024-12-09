9e1083dc2e690cad8853cba514262068054ab1e2
Alexandru Andronache <Alexandru@king.com>
2024-12-09 11:34:40 +0100

    2024 day 9; i should make it nicer but..

--- 2024/day09/main.cpp
+++ 2024/day09/main.cpp
@@ -1,13 +1,111 @@
 #include "file.h"
 #include <iostream>
+#include <utilities.h>
 
 namespace aoc2024_day09 {
-    int part_1(std::string_view path) {
-        return 0;
+    unsigned long long part_1(std::string_view path) {
+        unsigned long long sol = 0;
+        std::string input = file::readFileAsString(path);
+        std::vector<int> numbers;
+
+        int index = 0;
+        for (int i = 0; i < input.size(); i += 2) {
+            for (int j = 0; j < input[i] - '0'; ++j) {
+                numbers.push_back(index);
+            }
+            index++;
+            for (int j = 0; j < input[i + 1] - '0'; ++j) {
+                numbers.push_back(-1);
+            }
+        }
+
+        int start = 0;
+        for (int i = numbers.size() - 1; i >= 0; --i) {
+            if (numbers[i] != -1 && start <= i) {
+                while (start < numbers.size() && numbers[start] != -1) {
+                    start++;
+                }
+                if (start <= i) {
+                    std::swap(numbers[i], numbers[start]);
+                }
+            }
+        }
+
+        for (int i = 0; i < numbers.size(); ++i) {
+            if (numbers[i] != -1) {
+                sol += numbers[i] * i;
+            }
+        }
+
+        return sol;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long part_2(std::string_view path) {
+        unsigned long long sol = 0;
+        std::string input = file::readFileAsString(path);
+        std::vector<int> numbers;
+
+        int index = 0;
+        for (int i = 0; i < input.size(); i += 2) {
+            for (int j = 0; j < input[i] - '0'; ++j) {
+                numbers.push_back(index);
+            }
+            index++;
+            for (int j = 0; j < input[i + 1] - '0'; ++j) {
+                numbers.push_back(-1);
+            }
+        }
+
+        int i = numbers.size() - 1;
+        int id = numbers.back();
+        int start = 0;
+        while (i >= 0) {
+            start = 0;
+            while (i >= 0 && numbers[i] == -1) {
+                i--;
+            }
+            if (id < numbers[i]) {
+                i--;
+                continue;
+            }
+            id = numbers[i];
+            int startI = i;
+            int size = 1;
+            i--;
+            while (i >= 0 && numbers[i] != -1 && numbers[i + 1] == numbers[i]) {
+                size++;
+                i--;
+            }
+            int endI = i + 1;
+            bool found = false;
+            while (!found && start < numbers.size()) {
+                int begin = 0;
+                while (start < numbers.size() && numbers[start] != -1) {
+                    start++;
+                }
+                begin = start;
+                while (start < numbers.size() && numbers[start] == -1) {
+                    start++;
+                }
+                int end = start - 1;
+                if (end - begin + 1 >= size) {
+                    for (int k = startI; k >= endI; --k) {
+                        if (k >= begin + startI - k) {
+                            std::swap(numbers[k], numbers[begin + startI - k]);
+                        }
+                    }
+                    found = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < numbers.size(); ++i) {
+            if (numbers[i] != -1) {
+                sol += numbers[i] * i;
+            }
+        }
+
+        return sol;
     }
 }
 
--- 2024/day09/tests/TestClass.cpp
+++ 2024/day09/tests/TestClass.cpp
@@ -3,18 +3,18 @@
 
 namespace aoc2024_day09 {
     TEST_F(Tests2024Day09, part_1_test) {
-        ASSERT_EQ(part_1("../2024/day09/input_test.in"), 0);
+        ASSERT_EQ(part_1("../2024/day09/input_test.in"), 1928);
     }
 
     TEST_F(Tests2024Day09, part_1_real_test) {
-        ASSERT_EQ(part_1("../2024/day09/input.in"), 0);
+        ASSERT_EQ(part_1("../2024/day09/input.in"), 6323641412437);
     }
 
     TEST_F(Tests2024Day09, part_2_test) {
-        ASSERT_EQ(part_2("../2024/day09/input_test.in"), 0);
+        ASSERT_EQ(part_2("../2024/day09/input_test.in"), 2858);
     }
 
     TEST_F(Tests2024Day09, part_2_real_test) {
-        ASSERT_EQ(part_2("../2024/day09/input.in"), 0);
+        ASSERT_EQ(part_2("../2024/day09/input.in"), 6351801932670);
     }
 }
