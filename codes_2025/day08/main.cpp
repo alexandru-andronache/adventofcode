8053a02100bd2ca14182166184e793236cd9ebbc
Alexandru Andronache <Alexandru@king.com>
2025-11-19 19:01:12 +0100

    codes 2025 - day 8

--- codes_2025/day08/main.cpp
+++ codes_2025/day08/main.cpp
@@ -51,35 +51,29 @@ namespace aoccodes_2025_day08 {
     unsigned long long part_3(std::string_view path) {
         std::vector<long long> numbers = file::readFileAsArrayLongLong(path, ",");
         unsigned long long max = *std::max_element(numbers.begin(), numbers.end());
-        std::vector<std::pair<unsigned long long, unsigned long long>> pairs;
+        std::vector<std::vector<int>> grid(max + 1, std::vector<int>(max + 1, 0));
         for (int i = 0; i < numbers.size() - 1; i++) {
-            pairs.emplace_back(std::min(numbers[i], numbers[i + 1]),
-                std::max(numbers[i], numbers[i + 1]));
+            grid[numbers[i]][numbers[i + 1]]++;
+            grid[numbers[i + 1]][numbers[i]]++;
         }
 
         int sol = 0;
         for (int i = 1; i <= max; i++) {
             for (int j = i + 1; j <= max; j++) {
                 int count = 0;
-                for (const auto& pair : pairs) {
-                    if (i > pair.first && i < pair.second &&
-                        j > pair.second) {
-                        count++;
+                for (int k = i + 1; k < j; k++) {
+                    for (int l = 1; l < i; l++) {
+                        count += grid[k][l];
                     }
-                    if (j > pair.first && j < pair.second &&
-                        i < pair.first) {
-                        count++;
-                        }
+                    for (int l = j + 1; l <= max; l++) {
+                        count += grid[k][l];
+                    }
                 }
-                auto it = std::find_if(pairs.begin(), pairs.end(), [i, j](const auto& pair) {
-                    return pair.first == i && pair.second == j;
-                });
-                if (it != pairs.end()) {
-                    count++;
-                }
+                count += grid[i][j];
                 sol = std::max(sol, count);
             }
         }
+
         return sol;
     }
 }
