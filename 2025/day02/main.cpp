5ce76ddf4177cca5f6cefa89f411a32429ec5e7f
Alexandru Andronache <Alexandru@king.com>
2025-12-02 07:05:22 +0100

    2025 day 2 slow version

--- 2025/day02/main.cpp
+++ 2025/day02/main.cpp
@@ -9,38 +9,103 @@
 #endif
 
 namespace aoc2025_day02 {
-    int part_1(std::string_view path) {
-        // std::string input = file::readFileAsString(path);
-        std::vector<std::string> lines = file::readFileAsArrayString(path);
-        int result = 0;
+    unsigned long long part_1(std::string_view path) {
+        std::string input = file::readFileAsString(path);
+        unsigned long long result = 0;
 
-        for (const auto& line : lines) {
-            std::vector<std::string> tokens = utils::splitString(line, "");
+        std::vector<std::string> tokens = utils::splitString(input, "-,");
+
+        for (int i = 0; i < tokens.size(); i += 2) {
+            unsigned long long s = std::stoull(tokens[i]);
+            unsigned long long e = std::stoull(tokens[i + 1]);
+            for (unsigned long long v = s; v <= e; ++v) {
+                std::vector<int> nr;
+                unsigned long long c = v;
+                while (c > 0) {
+                    nr.push_back(c % 10);
+                    c /= 10;
+                }
+                if (nr.size() % 2 == 0) {
+                    int m = nr.size() / 2;
+                    bool valid = true;
+                    for (int j = 0; j < m; ++j) {
+                        if (nr[j] != nr[m + j]) {
+                            valid = false;
+                        }
+                    }
+                    if (valid) {
+                        result += v;
+                    }
+                }
+            }
         }
 
         return result;
     }
 
-    int part_2(std::string_view path) {
-        return 0;
+    unsigned long long part_2(std::string_view path) {
+        std::string input = file::readFileAsString(path);
+        unsigned long long result = 0;
+        std::map<unsigned long long, bool> prev;
+
+        std::vector<std::string> tokens = utils::splitString(input, "-,");
+
+        for (int i = 0; i < tokens.size(); i += 2) {
+            unsigned long long s = std::stoull(tokens[i]);
+            unsigned long long e = std::stoull(tokens[i + 1]);
+            for (unsigned long long v = s; v <= e; ++v) {
+                // if (prev.contains(v)) {
+                //     if (prev[v]) {
+                //         result += v;
+                //     }
+                //     break;
+                // }
+                std::vector<int> nr;
+                unsigned long long c = v;
+                while (c > 0) {
+                    nr.push_back(c % 10);
+                    c /= 10;
+                }
+                for (int k = 1; k <= nr.size() / 2; ++k) {
+                    bool valid = true;
+                    if (nr.size() % k == 0) {
+                        for (int j = 0; j < k && valid; ++j) {
+                            for (int l = 1; l < nr.size() / k && valid; ++l) {
+                                if (nr[j] != nr[j + l * k]) {
+                                    valid = false;
+                                }
+                            }
+                        }
+                        if (valid) {
+                            result += v;
+                            // prev.insert({v, true});
+                            break;
+                        }
+                    }
+                }
+                // prev.insert({v, false});
+            }
+        }
+
+        return result;
     }
 }
 
 #ifdef TESTING
     TEST(Test2025Day02, part_1_test) {
-        ASSERT_EQ(aoc2025_day02::part_1("../2025/day02/input_test.in"), 0);
+        ASSERT_EQ(aoc2025_day02::part_1("../2025/day02/input_test.in"), 1227775554);
     }
 
     TEST(Test2025Day02, part_1_real_test) {
-        ASSERT_EQ(aoc2025_day02::part_1("../2025/day02/input.in"), 0);
+        ASSERT_EQ(aoc2025_day02::part_1("../2025/day02/input.in"), 13108371860);
     }
 
     TEST(Test2025Day02, part_2_test) {
-        ASSERT_EQ(aoc2025_day02::part_2("../2025/day02/input_test.in"), 0);
+        ASSERT_EQ(aoc2025_day02::part_2("../2025/day02/input_test.in"), 4174379265);
     }
 
     TEST(Test2025Day02, part_2_real_test) {
-        ASSERT_EQ(aoc2025_day02::part_2("../2025/day02/input.in"), 0);
+        ASSERT_EQ(aoc2025_day02::part_2("../2025/day02/input.in"), 22471660255);
     }
 #endif
 
