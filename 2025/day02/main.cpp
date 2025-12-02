31f1d9131fe10c49bc7b37aa9154f198530e744d
Alexandru Andronache <Alexandru@king.com>
2025-12-02 20:44:30 +0100

    2025 day 2

--- 2025/day02/main.cpp
+++ 2025/day02/main.cpp
@@ -9,35 +9,40 @@
 #endif
 
 namespace aoc2025_day02 {
+    unsigned long long solve(const std::string& start, const std::string& end, int min, int max) {
+        std::set<unsigned long long> values;
+        unsigned long long sum = 0;
+        const unsigned long long from = std::stoull(start);
+        const unsigned long long to = std::stoull(end);
+
+        for (int p = min; p <= max; ++p) {
+            for (int j = static_cast<int>(std::ceil((float)start.size() / p)); j <= end.size() / p; ++j) {
+                unsigned long long s = std::pow(10, j - 1);
+                for (int k = s; k < s * 10; ++k) {
+                    unsigned long long nr = 0;
+                    for (int a = 0; a < p; ++a) {
+                        nr = nr * pow(10, j) + k;
+                    }
+                    if (nr >= from && nr <= to && !values.contains(nr)) {
+                        sum += nr;
+                        values.insert(nr);
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
+
     unsigned long long part_1(std::string_view path) {
         std::string input = file::readFileAsString(path);
         unsigned long long result = 0;
 
-        std::vector<std::string> tokens = utils::splitString(input, "-,");
+        auto ranges = utils::splitString(input, ",");
 
-        for (int i = 0; i < tokens.size(); i += 2) {
-            unsigned long long s = std::stoull(tokens[i]);
-            unsigned long long e = std::stoull(tokens[i + 1]);
-            for (unsigned long long v = s; v <= e; ++v) {
-                std::vector<int> nr;
-                unsigned long long c = v;
-                while (c > 0) {
-                    nr.push_back(c % 10);
-                    c /= 10;
-                }
-                if (nr.size() % 2 == 0) {
-                    int m = nr.size() / 2;
-                    bool valid = true;
-                    for (int j = 0; j < m; ++j) {
-                        if (nr[j] != nr[m + j]) {
-                            valid = false;
-                        }
-                    }
-                    if (valid) {
-                        result += v;
-                    }
-                }
-            }
+        for (const auto& range : ranges) {
+            std::vector<std::string> numbers = utils::splitString(range, "-");
+            result += solve(numbers[0], numbers[1], 2, 2);
         }
 
         return result;
@@ -46,45 +51,12 @@ namespace aoc2025_day02 {
     unsigned long long part_2(std::string_view path) {
         std::string input = file::readFileAsString(path);
         unsigned long long result = 0;
-        std::map<unsigned long long, bool> prev;
 
-        std::vector<std::string> tokens = utils::splitString(input, "-,");
+        std::vector<std::string> ranges = utils::splitString(input, ",");
 
-        for (int i = 0; i < tokens.size(); i += 2) {
-            unsigned long long s = std::stoull(tokens[i]);
-            unsigned long long e = std::stoull(tokens[i + 1]);
-            for (unsigned long long v = s; v <= e; ++v) {
-                // if (prev.contains(v)) {
-                //     if (prev[v]) {
-                //         result += v;
-                //     }
-                //     break;
-                // }
-                std::vector<int> nr;
-                unsigned long long c = v;
-                while (c > 0) {
-                    nr.push_back(c % 10);
-                    c /= 10;
-                }
-                for (int k = 1; k <= nr.size() / 2; ++k) {
-                    bool valid = true;
-                    if (nr.size() % k == 0) {
-                        for (int j = 0; j < k && valid; ++j) {
-                            for (int l = 1; l < nr.size() / k && valid; ++l) {
-                                if (nr[j] != nr[j + l * k]) {
-                                    valid = false;
-                                }
-                            }
-                        }
-                        if (valid) {
-                            result += v;
-                            // prev.insert({v, true});
-                            break;
-                        }
-                    }
-                }
-                // prev.insert({v, false});
-            }
+        for (const auto& range : ranges) {
+            std::vector<std::string> numbers = utils::splitString(range, "-");
+            result += solve(numbers[0], numbers[1], 2, numbers[1].size());
         }
 
         return result;
