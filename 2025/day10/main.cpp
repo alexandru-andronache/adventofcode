38bd872c81a6036107b699b390fad96ef17df768
Alexandru Andronache <Alexandru@king.com>
2025-12-11 05:41:58 +0100

    2025 day 10

--- 2025/day10/main.cpp
+++ 2025/day10/main.cpp
@@ -1,6 +1,9 @@
+#include <numeric>
+
 #include "file.h"
 #include "utilities.h"
 #include <vector>
+#include "z3++.h"
 
 #include <fmt/printf.h>
 
@@ -10,44 +13,125 @@
 
 namespace aoc2025_day10 {
     int part_1(std::string_view path) {
-        // std::string input = file::readFileAsString(path);
         std::vector<std::string> lines = file::readFileAsArrayString(path);
         int result = 0;
 
         for (const auto& line : lines) {
-            std::vector<std::string> tokens = utils::splitString(line, "");
+            std::vector<std::string> tokens = utils::splitString(line, " ");
+            std::vector<bool> state;
+            for (int i = 1; i < tokens[0].size() - 1; ++i) {
+                state.push_back(tokens[0][i] != '.');
+            }
+            std::vector<std::vector<int>> buttons;
+            for (int i = 1; i < tokens.size() - 1; i++) {
+                std::vector<int> button;
+                for (int j = 1; j < tokens[i].size() - 1; j += 2) {
+                    button.push_back(tokens[i][j] - '0');
+                }
+                buttons.push_back(button);
+            }
+
+            int min = INT_MAX;
+            for (int i = 0; i <= std::pow(2, buttons.size()); ++i) {
+                std::vector<bool> current_state(state.size(), false);
+                int nr = 0;
+                for (int j = 0; j < buttons.size() && nr < min; ++j) {
+                    if ((i >> j) & 1) {
+                        for (const auto& button : buttons[j]) {
+                            current_state[button] = !current_state[button];
+                        }
+                        nr++;
+                    }
+                }
+                bool valid = true;
+                for (int j = 0; j < current_state.size(); ++j) {
+                    if (current_state[j] != state[j]) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid && nr < min) {
+                    min = nr;
+                }
+            }
+            result += min;
         }
 
         return result;
     }
 
     int part_2(std::string_view path) {
-        return 0;
+        std::vector<std::string> lines = file::readFileAsArrayString(path);
+        int result = 0;
+
+        for (const auto& line : lines) {
+            std::vector<std::string> tokens = utils::splitString(line, " ");
+            std::vector<int> final_numbers = utils::splitStringToInt(tokens.back(), "{,}");
+            std::vector<std::vector<int>> combinations(final_numbers.size(), std::vector<int>());
+            for (int i = 1; i < tokens.size() - 1; i++) {
+                for (int j = 1; j < tokens[i].size() - 1; j += 2) {
+                    combinations[tokens[i][j] - '0'].push_back(i - 1);
+                }
+            }
+
+            z3::config cfg;
+            z3::context context(cfg);
+            std::vector<z3::expr> exprs;
+            for (int i = 0; i < tokens.size() - 2; ++i) {
+                exprs.push_back(context.int_const(("x_" + std::to_string(i)).c_str()));
+            }
+
+            z3::optimize solver(context);
+            for (int i = 0; i < combinations.size(); ++i) {
+                z3::expr lhs = context.int_val(0);
+                for (const auto& j : combinations[i]) {
+                    lhs = lhs + exprs[j];
+                }
+                solver.add(lhs == context.int_val(final_numbers[i]));
+            }
+
+            for (auto& x : exprs) {
+                solver.add(x >= 0);
+            }
+
+            z3::expr sum = context.int_val(0);
+            for (auto& x : exprs) {
+                sum = sum + x;
+            }
+            solver.minimize(sum);
+
+            solver.check();
+            result = std::accumulate(exprs.begin(), exprs.end(), result, [solver](const auto& total, const auto& x) {
+                return total + solver.get_model().eval(x).get_numeral_int();
+            });
+        }
+
+        return result;
     }
 }
 
 #ifdef TESTING
     TEST(Test2025Day10, part_1_test) {
-        ASSERT_EQ(aoc2025_day10::part_1("../2025/day10/input_test.in"), 0);
+        ASSERT_EQ(aoc2025_day10::part_1("../2025/day10/input_test.in"), 7);
     }
 
     TEST(Test2025Day10, part_1_real_test) {
-        ASSERT_EQ(aoc2025_day10::part_1("../2025/day10/input.in"), 0);
+        ASSERT_EQ(aoc2025_day10::part_1("../2025/day10/input.in"), 436);
     }
 
     TEST(Test2025Day10, part_2_test) {
-        ASSERT_EQ(aoc2025_day10::part_2("../2025/day10/input_test.in"), 0);
+        ASSERT_EQ(aoc2025_day10::part_2("../2025/day10/input_test.in"), 33);
     }
 
     TEST(Test2025Day10, part_2_real_test) {
-        ASSERT_EQ(aoc2025_day10::part_2("../2025/day10/input.in"), 0);
+        ASSERT_EQ(aoc2025_day10::part_2("../2025/day10/input.in"), 14999);
     }
 #endif
 
 #ifndef TESTING
 int main() {
     fmt::print("Part 1: {}\n", aoc2025_day10::part_1("../2025/day10/input.in"));
-    fmt::print("Part 2: {}\n", aoc2025_day10::part_1("../2025/day10/input.in"));
+    fmt::print("Part 2: {}\n", aoc2025_day10::part_2("../2025/day10/input.in"));
 
     return 0;
 }
--- aoc_common.cmake
+++ aoc_common.cmake
@@ -51,7 +51,6 @@ function(day_common DAY)
     include(${COMMON_SOURCES}/cmake/gtest.cmake)
 
     include(FetchContent)
-
     FetchContent_Declare(
       fmt
       GIT_REPOSITORY https://github.com/fmtlib/fmt
@@ -101,4 +100,79 @@ function(day_common DAY)
             general libgmock.a
             fmt::fmt
             )
+endfunction()
+
+function(day_common_with_z3 DAY)
+    include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
+    set(CMAKE_CXX_STANDARD 23)
+    SET(COMMON_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/../../common)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../../bin")
+
+    include(${COMMON_SOURCES}/cmake/gtest.cmake)
+
+    include(FetchContent)
+    FetchContent_Declare(Z3
+            GIT_REPOSITORY https://github.com/Z3Prover/z3
+            GIT_TAG        z3-4.15.3
+    )
+    FetchContent_MakeAvailable(Z3)
+
+    if(TARGET libz3)
+        target_include_directories(libz3 INTERFACE
+                $<BUILD_INTERFACE:${z3_SOURCE_DIR}/src/api/c++>
+        )
+    endif()
+
+    include(FetchContent)
+    FetchContent_Declare(
+            fmt
+            GIT_REPOSITORY https://github.com/fmtlib/fmt
+            GIT_TAG        11.1.3)
+    FetchContent_MakeAvailable(fmt)
+
+    file(GLOB utils_src "${COMMON_SOURCES}/src/*.cpp")
+
+    file(GLOB cpp_files "${CMAKE_CURRENT_SOURCE_DIR}/*.cpp")
+    file(GLOB h_files "${CMAKE_CURRENT_SOURCE_DIR}/*.h")
+
+    file(GLOB cpp_files_test "${CMAKE_CURRENT_SOURCE_DIR}/tests/*.cpp")
+    file(GLOB h_files_test "${CMAKE_CURRENT_SOURCE_DIR}/tests/*.h")
+
+    add_executable(main main.cpp ${utils_src})
+    add_executable(test main.cpp ${cpp_files} ${h_files} ${utils_src} ${cpp_files_test} ${h_files_test} ${COMMON_SOURCES}/test.cpp)
+    add_executable(test.sanitize main.cpp ${cpp_files} ${h_files} ${utils_src} ${cpp_files_test} ${h_files_test} ${COMMON_SOURCES}/test.cpp)
+
+    target_include_directories(main PRIVATE ${COMMON_SOURCES}/include)
+    target_include_directories(test PRIVATE ${COMMON_SOURCES}/include)
+    target_include_directories(test.sanitize PRIVATE ${COMMON_SOURCES}/include)
+
+    add_dependencies(test googletest_utilities)
+    add_dependencies(test.sanitize googletest_utilities)
+
+    target_compile_options(test.sanitize PRIVATE -fsanitize=address -fsanitize=undefined -Wall -Werror -Wextra -Wsign-conversion -Wconversion -Wpedantic -Wshadow)
+    target_link_options(test.sanitize PRIVATE -fsanitize=address -fsanitize=undefined -Wall -Werror -Wextra -Wsign-conversion -Wconversion -Wpedantic -Wshadow)
+
+    target_compile_definitions(test PRIVATE TESTING=1)
+    target_compile_definitions(test.sanitize PRIVATE TESTING=1)
+
+    target_link_libraries(main
+            fmt::fmt
+            libz3)
+
+    target_link_libraries(test
+            general pthread
+            general c++
+            general libgtest.a
+            general libgmock.a
+            fmt::fmt
+            libz3
+    )
+
+    target_link_libraries(test.sanitize
+            general pthread
+            general c++
+            general libgtest.a
+            general libgmock.a
+            fmt::fmt
+    )
 endfunction()
