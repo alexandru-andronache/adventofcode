6727f09e127a259a709efbf6bc879a3bef857915
Alexandru Andronache <Alexandru@king.com>
2025-12-12 06:38:09 +0100

    2025 day 12

--- 2025/day12/main.cpp
+++ 2025/day12/main.cpp
@@ -9,13 +9,41 @@
 #endif
 
 namespace aoc2025_day12 {
+    struct shape {
+        std::array<std::array<bool, 3>, 3> matrix;
+        int total = 0;
+    };
+
     int part_1(std::string_view path) {
-        // std::string input = file::readFileAsString(path);
         std::vector<std::string> lines = file::readFileAsArrayString(path);
         int result = 0;
 
-        for (const auto& line : lines) {
-            std::vector<std::string> tokens = utils::splitString(line, "");
+        std::vector<shape> shapes;
+        for (int k = 0; k < lines.size(); ++k) {
+            if (lines[k][1] == ':') {
+                shape s;
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        s.matrix[i][j] = (lines[k + 1 + i][j] == '#');
+                        s.total += s.matrix[i][j];
+                    }
+                }
+                shapes.push_back(s);
+                k += 4;
+            }
+            else {
+                std::vector<int> tokens = utils::splitStringToInt(lines[k], " :x,");
+
+                int w = tokens[0];
+                int h = tokens[1];
+                int s = 0;
+                for (int i = 2; i < tokens.size(); ++i) {
+                    s += shapes[i - 2].total * tokens[i];
+                }
+                if (s < w * h) {
+                    result++;
+                }
+            }
         }
 
         return result;
@@ -27,20 +55,9 @@ namespace aoc2025_day12 {
 }
 
 #ifdef TESTING
-    TEST(Test2025Day12, part_1_test) {
-        ASSERT_EQ(aoc2025_day12::part_1("../2025/day12/input_test.in"), 0);
-    }
 
     TEST(Test2025Day12, part_1_real_test) {
-        ASSERT_EQ(aoc2025_day12::part_1("../2025/day12/input.in"), 0);
-    }
-
-    TEST(Test2025Day12, part_2_test) {
-        ASSERT_EQ(aoc2025_day12::part_2("../2025/day12/input_test.in"), 0);
-    }
-
-    TEST(Test2025Day12, part_2_real_test) {
-        ASSERT_EQ(aoc2025_day12::part_2("../2025/day12/input.in"), 0);
+        ASSERT_EQ(aoc2025_day12::part_1("../2025/day12/input.in"), 510);
     }
 #endif
 
